3cfabc638006228da467241437bf857184e2308c
Koen Wolters <koen.wolters@cern.ch>
2017-03-16 16:16:01 +0100

    removed eigen extensions due to include problems

--- src/tools/eigen_matrix_extend.h
+++ src/tools/eigen_matrix_extend.h
@@ -1,10 +1,20 @@
+/*#ifdef HAVE_GEANT
 #include <G4ThreeVector.hh>
+#endif
+
+#ifdef HAVE_ROOT
 #include <Math/PositionVector3D.h>
 #include <Math/SVector.h>
 #include <TVector3.h>
+#endif*/
 
+#ifdef HAVE_GEANT
 operator G4ThreeVector() const { return G4ThreeVector(this->operator()(0), this->operator()(1), this->operator()(2)); }
+#endif
+
+#ifdef HAVE_ROOT
 operator TVector3() const { return TVector3(this->operator()(0), this->operator()(1), this->operator()(2)); }
+
 operator ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>>() const {
     return ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<double>>(
         this->operator()(0), this->operator()(1), this->operator()(2));
@@ -16,3 +26,4 @@ template <typename T, unsigned int D> operator ROOT::Math::SVector<T, D>() const
     }
     return svec;
 }
+#endif
