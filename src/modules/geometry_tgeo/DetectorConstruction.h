31cc6c159581a610e6f5a5c32b03538fb31528b7
Koen Wolters <koen.wolters@cern.ch>
2017-03-17 14:50:02 +0100

    make everything compile with full warnings, start integrating

--- src/modules/geometry_tgeo/DetectorConstruction.h
+++ src/modules/geometry_tgeo/DetectorConstruction.h
@@ -7,26 +7,27 @@
 /// \version  0.9
 /// \author N. Gauvin; CERN
 
-#ifndef DetectorConstruction_h
-#define DetectorConstruction_h 1
+#ifndef ALLPIX_DETECTOR_CONSTRUCTION_TGEO_H
+#define ALLPIX_DETECTOR_CONSTRUCTION_TGEO_H
 
-#include <vector>
 #include <map>
+#include <memory>
+#include <vector>
 
 // ROOT
-#include "TGeoManager.h"
-
+#include <TGeoManager.h>
+#include <TVector3.h>
 
 // AllPix2
-#include "GeoDsc.h"
+#include "core/geometry/PixelDetectorModel.hpp"
 
 /*** Names of detector parts
  * These are extremely important and should be placed in a visible way,
- * as they will be used to retrieve the objects from the gGeoManager. 
+ * as they will be used to retrieve the objects from the gGeoManager.
  ***/
 const TString WrapperName = "Wrapper";
 const TString PCBName = "PCB";
-const TString WaferName = "Wafer"; //Box in AllPix1
+const TString WaferName = "Wafer"; // Box in AllPix1
 const TString CoverName = "Coverlayer";
 const TString SliceName = "Slice";
 const TString PixelName = "Pixel";
@@ -34,41 +35,42 @@ const TString ChipName = "Chip";
 const TString BumpName = "Bump";
 const TString GuardRingsName = "GuardRings";
 
+namespace allpix {
 
-class DetectorConstruction
-{
+    class DetectorConstruction {
 
- public:
-  DetectorConstruction();
-  ~DetectorConstruction();
+    public:
+        DetectorConstruction();
+        ~DetectorConstruction();
 
-  void run();
-  void Construct();
-  void BuildPixelDevices();
-  void BuildMaterialsAndMedia();
-  void BuildAppliances();
-  void BuildTestStructure();
-  void ReadDetectorDescriptions(); /// Debugging only !
-  
-  // Global variables
-  TGeoMedium* m_fillingWorldMaterial; /// Medium to fill the World.
-  vector<GeoDsc*> m_geoMap; /// the detector descriptions
-  
-  // User defined parameters
-  /*
-    Medium to fill the World. Available media :
-    - Air
-    - Vacuum
-  */
-  TString m_userDefinedWorldMaterial; 
-  TString m_userDefinedGeoOutputFile;
-  TString m_buildAppliancesFlag;
-  int m_Appliances_type;
-  TString m_buildTestStructureFlag;
-  map<int,TVector3> m_vectorWrapperEnhancement;
-  map<int,TGeoTranslation> m_posVector; // position of medipix(es), key is detector Id
-  map<int,TGeoRotation> m_rotVector; // map<int, G4RotationMatrix *>
-  map<int,TGeoTranslation> m_posVectorAppliances; // 
-};
+        void run();
+        void Construct();
+        void BuildPixelDevices();
+        void BuildMaterialsAndMedia();
+        void BuildAppliances();
+        void BuildTestStructure();
+        void ReadDetectorDescriptions(); /// Debugging only !
 
-#endif /*DetectorConstruction_h*/
+        // Global variables
+        TGeoMedium*                                      m_fillingWorldMaterial; /// Medium to fill the World.
+        std::vector<std::shared_ptr<PixelDetectorModel>> m_geoMap;               /// the detector descriptions
+
+        // User defined parameters
+        /*
+          Medium to fill the World. Available media :
+          - Air
+          - Vacuum
+        */
+        TString m_userDefinedWorldMaterial;
+        TString m_userDefinedGeoOutputFile;
+        bool    m_buildAppliancesFlag;
+        int     m_Appliances_type;
+        bool    m_buildTestStructureFlag;
+        std::map<int, TVector3>        m_vectorWrapperEnhancement;
+        std::map<int, TGeoTranslation> m_posVector;           // position of medipix(es), key is detector Id
+        std::map<int, TGeoRotation>    m_rotVector;           // map<int, G4RotationMatrix *>
+        std::map<int, TGeoTranslation> m_posVectorAppliances; //
+    };
+}
+
+#endif /* ALLPIX_DETECTOR_CONSTRUCTION_TGEO_H */
--- src/modules/geometry_tgeo/LoadMe.C
+++
@@ -1,4 +0,0 @@
-{
-  gROOT->ProcessLine(".L GeoDsc.cpp++");
-  gROOT->ProcessLine(".L DetectorConstruction.cpp++");
-}
