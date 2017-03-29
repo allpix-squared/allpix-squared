89315adb3f74c2142ebe038ca7aa5fc098a063bd
Koen Wolters <koen.wolters@cern.ch>
2017-03-29 13:48:32 +0200

    fix install hook script

--- etc/git-hooks/install-hooks.sh
+++ etc/git-hooks/install-hooks.sh
@@ -7,12 +7,12 @@ ROOT=$(git rev-parse --show-toplevel)
 if [ -f $ROOT/.git/hooks/pre-commit-clang-format ];then
     echo "WARNING: File .git/hooks/pre-commit-clang-format already exists! Overwriting previous version..."
 fi
-cp $ROOT/tools/pre-commit-clang-format-hook $ROOT/.git/hooks/pre-commit-clang-format
+cp $ROOT/etc/git-hooks/pre-commit-clang-format-hook $ROOT/.git/hooks/pre-commit-clang-format
 chmod +x $ROOT/.git/hooks/pre-commit-clang-format
 
 if [ -f $ROOT/.git/hooks/pre-commit ];then
     echo "ERROR: File .git/hooks/pre-commit already exists! Will not overwrite, add script manually if needed."
 else
-    cp $ROOT/tools/pre-commit-hook $ROOT/.git/hooks/pre-commit
+    cp $ROOT/etc/git-hooks/pre-commit-hook $ROOT/.git/hooks/pre-commit
     chmod +x $ROOT/.git/hooks/pre-commit
 fi
