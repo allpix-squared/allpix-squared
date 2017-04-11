59ac715ddf528cef0739754ac60cc571cf1f98f9
Koen Wolters <koen.wolters@cern.ch>
2017-04-11 17:51:24 +0200

    add module name to output section

--- src/core/module/ModuleManager.cpp
+++ src/core/module/ModuleManager.cpp
@@ -15,7 +15,15 @@ ModuleManager::~ModuleManager() = default;
 void ModuleManager::init() {
     // initialize all modules
     for(auto& mod : modules_) {
+        // set init module section header
+        std::string old_section_name = Log::getSection();
+        std::string section_name = "I:";
+        section_name += module_to_id_.at(mod.get()).getName();
+        Log::setSection(section_name);
+        // init module
         mod->init();
+        // reset header
+        Log::setSection(old_section_name);
     }
 }
 
@@ -24,10 +32,18 @@ void ModuleManager::run() {
     // loop over the number of events
     auto number_of_events = global_config_.get<unsigned int>("number_of_events", 1u);
     for(unsigned int i = 0; i < number_of_events; ++i) {
-        LOG(DEBUG) << "Running event " << (i + 1) << " of " << number_of_events;
         // go through each module run method every event
+        LOG(DEBUG) << "Running event " << (i + 1) << " of " << number_of_events;
         for(auto& mod : modules_) {
+            // set run module section header
+            std::string old_section_name = Log::getSection();
+            std::string section_name = "R:";
+            section_name += module_to_id_.at(mod.get()).getName();
+            Log::setSection(section_name);
+            // run module
             mod->run();
+            // reset header
+            Log::setSection(old_section_name);
         }
     }
 }
@@ -36,6 +52,14 @@ void ModuleManager::run() {
 void ModuleManager::finalize() {
     // finalize the modules
     for(auto& mod : modules_) {
+        // set finalize module section header
+        std::string old_section_name = Log::getSection();
+        std::string section_name = "F:";
+        section_name += module_to_id_.at(mod.get()).getName();
+        Log::setSection(section_name);
+        // finalize module
         mod->finalize();
+        // reset header
+        Log::setSection(old_section_name);
     }
 }
