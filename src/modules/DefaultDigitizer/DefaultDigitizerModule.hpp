de736bb0043c714a17f98497274095dd524d8351
Simon Spannagel <simon.spannagel@cern.ch>
2017-05-18 15:03:23 +0200

    Add some dicumentation to DefaultDigitizer

--- src/modules/DefaultDigitizer/DefaultDigitizerModule.hpp
+++ src/modules/DefaultDigitizer/DefaultDigitizerModule.hpp
@@ -18,19 +18,36 @@
 #include <TProfile.h>
 
 namespace allpix {
-    // define the module inheriting from the module base class
+    /**
+      * @brief Module to simulate digitization of collected charges
+      *
+      * This module provides a relatively simple simulation of the frontend electronics behavior. It simulates the
+     * propagation of the signal of collected charges through the amplifier, comparator and ADC while adding electronics
+     * noise and simulating the threshold as well as accounting for threshold dispersion and ADC noise.
+      */
     class DefaultDigitizerModule : public Module {
     public:
-        // constructor should take a pointer to the Configuration, the Messenger and the Geometry Manager
-        DefaultDigitizerModule(Configuration, Messenger*, std::shared_ptr<Detector>);
+        /**
+           * @brief Constructor for the DefaultDigitizerModule, inheriting from the base class allpix::Module
+           * @param config configuration object for this module as retrieved from the steering file
+           * @param messenger pointer to the messenger object to allow binding to messages on the bus
+       * @param detector pointer to the detector for this module instance
+           */
+        DefaultDigitizerModule(Configuration config, Messenger* messenger, std::shared_ptr<Detector> detector);
 
-        // init debug plots
+        /**
+         * @brief Initialize ROOT histograms and ROOT output file
+         */
         void init() override;
 
-        // simulate digitization process:
+        /**
+         * @brief Simulate digitization process
+         */
         void run(unsigned int) override;
 
-        // write the histograms
+        /**
+         * @brief Finalize histogramming and write the to the output file
+         */
         void finalize() override;
 
     private:
@@ -40,10 +57,10 @@ namespace allpix {
         // configuration for this module
         Configuration config_;
 
-        // messenger to emit test message
+        // messenger to bind to input message and dispatch output message
         Messenger* messenger_;
 
-        // deposits for a specific detector
+        // retrieved message containing collected charges per pixel for a specific detector
         std::shared_ptr<PixelChargeMessage> pixel_message_;
 
         // ROOT file for output plots
