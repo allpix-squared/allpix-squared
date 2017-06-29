9a2824593d550ec341e31feafcf23033e4423875
Salman Maqbool <smaqbool@cern.ch>
2017-06-29 17:32:11 +0200

    Added RCEWriter module

---
+++ src/modules/RCEWriter/RCEWriterModule.cpp
@@ -0,0 +1,122 @@
+#include "RCEWriterModule.hpp"
+
+#include <string>
+#include <utility>
+
+#include <TBranchElement.h>
+#include <TClass.h>
+#include <TDirectory.h>
+
+#include "core/utils/log.h"
+#include "core/utils/type.h"
+
+#include "objects/Object.hpp"
+#include "objects/objects.h"
+
+using namespace allpix;
+
+RCEWriterModule::RCEWriterModule(Configuration config, Messenger* messenger, GeometryManager* geo_mgr)
+    : Module(config), config_(std::move(config)), geo_mgr_(geo_mgr) {
+    // Bind to all messages
+    //  messenger->bindMulti(this, &RCEWriterModule::pixel_charge_messages_, MsgFlags::REQUIRED);
+    messenger->bindMulti(this, &RCEWriterModule::pixel_hit_messages_, MsgFlags::REQUIRED);
+}
+RCEWriterModule::~RCEWriterModule() = default;
+
+void RCEWriterModule::init() {
+    // Create output file
+    std::string file_name = getOutputPath(config_.get<std::string>("file_name", "data") + ".root", true);
+    output_file_ = std::make_unique<TFile>(file_name.c_str(), "RECREATE");
+    output_file_->cd();
+
+    // Initialize the events tree
+    event_tree_ = std::make_unique<TTree>("Event", "");
+    event_tree_->Branch("TimeStamp", &timestamp_);
+    event_tree_->Branch("FrameNumber", &frame_number_);
+    event_tree_->Branch("TriggerOffset", &trigger_offset_);
+    event_tree_->Branch("TriggerInfo", &trigger_info_);
+    event_tree_->Branch("TriggerTime", &trigger_time_);
+    event_tree_->Branch("Invalid", &invalid_);
+
+    // Get the detector names
+    for(const auto& detector : geo_mgr_->getDetectors()) {
+        detector_names_.push_back(detector->getName());
+    }
+    // Sort the detector names
+    std::sort(detector_names_.begin(), detector_names_.end());
+    // For each detector name, initialze an instance of SensorData
+    for(const auto& detector_name : detector_names_) {
+        auto& sensor = sensors_[detector_name];
+        // Create directories for each detector
+        TDirectory* detector = output_file_->mkdir(detector_name.c_str());
+        detector->cd();
+
+        // Initialize the struct for each detector
+        sensor.tree = std::make_unique<TTree>("Hits", "");
+        LOG(TRACE) << "Detector name is: " << detector_name;
+        // initialze tree branches for each instance of the sensorData
+        sensor.tree->Branch("NHits", &sensor.nhits_);
+        sensor.tree->Branch("PixX", &sensor.pix_x_, "PixX[NHits]/I");
+        sensor.tree->Branch("PixY", &sensor.pix_y_, "PixY[NHits]/I");
+        sensor.tree->Branch("Value", &sensor.value_, "Value[NHits]/I");
+        sensor.tree->Branch("Timing", &sensor.timing_, "Timing[NHits]/I");
+        sensor.tree->Branch("HitInCluster", &sensor.hit_in_cluster_, "HitInCluster[NHits]/I");
+    }
+}
+
+void RCEWriterModule::run(unsigned int event_id) {
+    // fill per-event data
+    timestamp_ = 0;
+    frame_number_ = event_id;
+    trigger_offset_ = 0;
+    trigger_info_ = 0;
+    trigger_time_ = 0;
+    invalid_ = false;
+
+    LOG(TRACE) << "Writing new objects to the Events tree";
+    event_tree_->Fill();
+
+    // Loop over all the detectors
+    for(const auto& detector_name : detector_names_) {
+        // reset nhits
+        auto& sensor = sensors_[detector_name];
+        sensor.nhits_ = 0;
+    }
+
+    // Loop over the pixel hit messages
+    for(const auto& hit_msg : pixel_hit_messages_) {
+
+        std::string detector_name = hit_msg->getDetector()->getName();
+        LOG(TRACE) << "Detector Name: " << detector_name;
+        auto& sensor = sensors_[detector_name];
+
+        // Loop over all the hits
+        for(const auto& hit : hit_msg->getData()) {
+            int i = sensor.nhits_;
+
+            // Get the pixel charge
+            sensor.nhits_ += 1;
+            sensor.pix_x_[i] = hit.getPixel().x();                  // NOLINT
+            sensor.pix_y_[i] = hit.getPixel().y();                  // NOLINT
+            sensor.value_[i] = static_cast<Int_t>(hit.getSignal()); // NOLINT
+            // Set the  Timing and HitInCluster for each sesnor_tree (= 0 for now)
+            sensor.timing_[i] = 0;         // NOLINT
+            sensor.hit_in_cluster_[i] = 0; // NOLINT
+
+            LOG(TRACE) << "X: " << hit.getPixel().x();
+            LOG(TRACE) << "Y: " << hit.getPixel().y();
+            LOG(TRACE) << "Signal: " << hit.getSignal();
+        }
+    }
+
+    // Loop over all the detectors
+    for(const auto& detector_name : detector_names_) {
+        LOG(TRACE) << "Writing new objects to the Sensor Tree for " << detector_name;
+        sensors_[detector_name].tree->Fill();
+    }
+}
+
+void RCEWriterModule::finalize() {
+    LOG(TRACE) << "Writing objects to file";
+    output_file_->Write();
+}
---
+++ src/modules/RCEWriter/RCEWriterModule.hpp
@@ -0,0 +1,63 @@
+#include <map>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include "core/config/Configuration.hpp"
+
+#include "core/geometry/GeometryManager.hpp"
+#include "core/messenger/Messenger.hpp"
+
+#include "core/module/Module.hpp"
+#include "objects/PixelCharge.hpp"
+#include "objects/PixelHit.hpp"
+
+namespace allpix {
+    class RCEWriterModule : public Module {
+    public:
+        RCEWriterModule(Configuration config, Messenger*, GeometryManager*);
+        ~RCEWriterModule() override;
+
+        // Open the ROOT file to write to
+        void init() override;
+
+        // Write current event messages to the output
+        void run(unsigned int) override;
+
+        // Finalize the reading
+        void finalize() override;
+
+    private:
+        Configuration config_;
+
+        GeometryManager* geo_mgr_;
+
+        std::vector<std::string> detector_names_;
+
+        struct SensorData {
+            std::unique_ptr<TTree> tree;
+            Int_t nhits_;
+            Int_t pix_x_[1024];
+            Int_t pix_y_[1024];
+            Int_t value_[1024];
+            Int_t timing_[1024];
+            Int_t hit_in_cluster_[1024];
+        };
+
+        std::unique_ptr<TFile> output_file_;
+        std::unique_ptr<TTree> event_tree_;
+        std::map<std::string, SensorData> sensors_;
+
+        ULong64_t timestamp_;
+        ULong64_t frame_number_;
+        Int_t trigger_offset_;
+        Int_t trigger_info_;
+        Int_t trigger_time_;
+        Bool_t invalid_;
+
+        // retrieved message containing collected charges per pixel for a specific detector
+        // std::vector<std::shared_ptr<PixelChargeMessage>> pixel_charge_messages_;
+        std::vector<std::shared_ptr<PixelHitMessage>> pixel_hit_messages_;
+    };
+} // namespace allpix
