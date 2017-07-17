0e844363910e799d0490b5b15f19a1cbf1783a81
Simon Spannagel <simon.spannagel@cern.ch>
2017-07-17 17:22:43 +0200

    Add script to create new module directory and README

--- etc/scripts/README.md
+++ etc/scripts/README.md
@@ -1,10 +1,48 @@
 # Tools
 
+## make_module.sh
+
+Short script to prepare the source code and CMake files for a new allpix<sup>2</sup> module. 
+The tool will ask for all necessary information, such as the module name ("Module" will be appended automatically) and the type.
+Both unique and detector-specific modules are supported.
+
+A README.md file is automatically created, containing the author and contact information of the creator. The information is retrieved using the `git config user.name` and `git config user.email` commands if available, or `whoami` and `hostname` as fallback.
+Please make sure, the information is correct.
+
+Usage:
+
+```
+etc/scripts/make_module.sh
+```
+
+Example output:
+
+```
+$ ../etc/make_module.sh 
+
+Preparing code basis for a new module:
+
+Name of the module? MyNewPropagator
+Type of the module?
+1) unique
+2) detector
+#? 1
+Creating directory and files...
+
+Name:   MyNewPropagator
+Author: John Doe (john.doe AT cern.ch)
+Path:   ../src/modules/MyNewPropagator
+
+Re-run CMake in order to build your new module.
+
+```
+
+
 ## print_module_parameters.sh
 
 Simple tool which parses the source code of a module and prints a list of all available configuration parameters together with their type. It takes the source code directory of the respective module as command line argument.
 
-Exmaple usage:
+Example usage:
 
 ```
 etc/scripts/print_module_parameters.sh src/modules/DefaultDigitizer/
---
+++ etc/scripts/make_module.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo -e "\nPreparing code basis for a new module:\n"
+
+# Ask for module name:
+read -p "Name of the module? " MODNAME
+
+# Ask for module type:
+echo "Type of the module?"
+type=0
+select yn in "unique" "detector"; do
+    case $yn in
+        unique ) type=1; break;;
+        detector ) type=2; break;;
+    esac
+done
+
+echo "Creating directory and files..."
+
+echo 
+# Try to find the modules directory:
+DIRECTORIES[0]="../src/modules"
+DIRECTORIES[1]="src/modules"
+
+MODDIR=""
+for DIR in "${DIRECTORIES[@]}"; do
+    if [ -d "$DIR" ]; then
+	MODDIR="$DIR"
+	break
+    fi
+done
+
+# Create directory
+mkdir "$MODDIR/$MODNAME"
+
+# Copy over CMake file and sources from Dummy:
+sed -e "s/Dummy/$MODNAME/g" "$MODDIR/Dummy/CMakeLists.txt" > "$MODDIR/$MODNAME/CMakeLists.txt" 
+
+# Copy over the README, setting current git username/email as author
+# If this fails, use system username and hostname
+MYNAME=$(git config user.name)
+MYMAIL=$(git config user.email)
+if [ -z "$MYNAME" ]; then
+    MYNAME=$(whoami)
+fi
+if [ -z "$MYMAIL" ]; then
+    MYMAIL=$(hostname)
+fi
+sed -e "s/Dummy/$MODNAME/g" \
+    -e "s/\*NAME\*/$MYNAME/g" \
+    -e "s/\*EMAIL\*/$MYMAIL/g" \
+    -e "s/Functional/Immature/g" \
+    "$MODDIR/Dummy/README.md" > "$MODDIR/$MODNAME/README.md.txt"
+
+# Copy over source code skeleton:
+sed -e "s/Dummy/$MODNAME/g" "$MODDIR/Dummy/DummyModule.hpp" > "$MODDIR/$MODNAME/${MODNAME}Module.hpp" 
+sed -e "s/Dummy/$MODNAME/g" "$MODDIR/Dummy/DummyModule.cpp" > "$MODDIR/$MODNAME/${MODNAME}Module.cpp" 
+
+# Change to detetcor module type if necessary:
+if [ "$type" == 2 ]; then
+    sed -i -e "s/_UNIQUE_/_DETECTOR_/g" "$MODDIR/$MODNAME/CMakeLists.txt"
+    sed -i -e "s/ unique / detector-specific /g" \
+	-e "/param geo/c\         \* \@param detector Pointer to the detector for this module instance" \
+	-e "s/GeometryManager\* geo\_manager/std::shared\_ptr\<Detector\> detector/g" \
+	-e "s/GeometryManager/DetectorModel/g" \
+	"$MODDIR/$MODNAME/${MODNAME}Module.hpp"
+    sed -i -e "s/GeometryManager\*/std::shared\_ptr\<Detector\> detector/g" \
+	-e "s/Module(config)/Module\(config\, detector\)/g" \
+	"$MODDIR/$MODNAME/${MODNAME}Module.cpp"
+fi
+
+echo "Name:   $MODNAME"
+echo "Author: $MYNAME ($MYMAIL)"
+echo "Path:   $MODDIR/$MODNAME"
+echo
+echo "Re-run CMake in order to build your new module."
