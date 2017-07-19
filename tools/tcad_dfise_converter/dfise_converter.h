8ca988252ba90adad7992b94eff0cc09eb1da9d1
Mateus Vicente <mvicente@cern.ch>
2017-07-19 18:18:12 +0200

    barycentric interpolation implemented

--- tools/tcad_dfise_converter/dfise_converter.h
+++ tools/tcad_dfise_converter/dfise_converter.h
@@ -1,2 +1,5 @@
+#include "read_dfise.h"
 // Main function to execute converter
 int main(int argc, char** argv);
+Point barycentric_interpolation(Point query_point, std::vector<Point> tetra_vertices,
+		                                   std::vector<Point> tetra_vertices_field, double tetra_volume);
