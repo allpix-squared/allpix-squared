a293c598676c94100179e5c4eff893e98e0714d6
Koen Wolters <koen.wolters@cern.ch>
2017-07-27 10:40:10 +0200

    updates to the mesh plotter

--- tools/tcad_dfise_converter/README.md
+++ tools/tcad_dfise_converter/README.md
@@ -1,8 +1,9 @@
 ## TCAD DF-ISE mesh converter
-This code takes as input the .grd and .dat files from the TCAD simulations. The .grd file contains the vertex coordinates (3D or 2D) of each mesh node and the .dat file contains the module of each electric field vector component for each mesh node, grouped by model regions (such as silicon bulk or metal contacts). The regions are defined in the .grd file by grouping vertices into edges, faces and, consecutively, volumes or elements.
-A new regular mesh is created by scanning the model volume in regular X Y and Z steps (not coinciding necessarily with original mesh nodes) and using a barycentric interpolation method to calculate the respective electric field vector on the new point. The interpolation uses the 4 closest, no-coplanar, neighbour vertex nodes that respective tetrahedron encloses the query point. 
-For the neighbours search, the software uses the Octree implementation from the paper "Efficient Radius Neighbor Search in Three-dimensional Point Clouds" by J. Behley et al.
-The output .init file (with the same .grd and .dat prefixe) is the INIT file to be used by allpix2. It contains the header of the INIT file folowed by a list with the columns organized as
+This code takes as input the .grd and .dat files from TCAD simulations. The .grd file contains the vertex coordinates (3D or 2D) of each mesh node and the .dat file contains the module of each electric field vector component for each mesh node, grouped by model regions (such as silicon bulk or metal contacts). The regions are defined in the .grd file by grouping vertices into edges, faces and, consecutively, volumes or elements.
+
+A new regular mesh is created by scanning the model volume in regular X Y and Z steps (not coinciding necessarily with original mesh nodes) and using a barycentric interpolation method to calculate the respective electric field vector on the new point. The interpolation uses the 4 closest, no-coplanar, neighbour vertex nodes that respective tetrahedron encloses the query point. For the neighbours search, the software uses the Octree implementation from the paper "Efficient Radius Neighbor Search in Three-dimensional Point Clouds" by J. Behley et al (see below).
+
+The output .init file (with the same .grd and .dat prefixe) can be imported into allpix (see the User's Manual for details). The INIT file has a header followed by a list of columns organized as
 ```bash
 node.x	node.y	node.z	e-field.x	e-field.y	e-field.z
 ```
@@ -16,9 +17,9 @@ node.x	node.y	node.z	e-field.x	e-field.y	e-field.z
 
 #### Usage
 Example .grd and .dat files can be found in the data folder with the example_data prefix.
-To run the program you should do:
+To run the program the following should be executed from the installation folder:
 ```bash
-./tcad_dfise_converter -f <file_name_prefix> [<options>] [<arguments>]
+bin/tcad_dfise_converter/dfise_converter -f <file_name_prefix> [<options>] [<arguments>]
 ```
 The list with options can be accessed using the -h option.
 Default values are assumed for the options not used. These are
@@ -27,19 +28,17 @@ Default values are assumed for the options not used. These are
 -r <radius step>	= 0.5 um
 -m <max radius>		= 10 um
 -c <volume cut>		= std::numeric_limits<double>::min()
--x,y,z <mesh binning>	= 100 (NOTE: option should be -x, -y or -z)
+-x,y,z <mesh binning>	= 100 (option should be set using -x, -y and -z)
 
 The output INIT file will be named with the same file_name_prefix as the .grd and .dat files.
 
-INIT files, with default name <file_name_prefix>_<x.bin>_<y.bin>_<z.bin>.init, can be read with the mesh_plot tool doing
+INIT files are read by specifying a file_name containing an .INIT file. The mesh_plotter tool can be used from the installation folder as follows:
 ```bash
-./mesh_plot -f <file_name_prefix> [<options>] [<arguments>]
+bin/tcad_dfise_converter/mesh_plotter -f <file_name> [<options>] [<arguments>]
 ```
-The list with options is also shown with the -h option.
-For the file name, the default value is 100 for the binning.
-In a 3D mesh, the plane to be ploted must be identified by using the option -p with agument "xy", "yz" or "zx".
-The data to be ploted can be selected with the -d option.
-In a .init file containing the components of the electric field, the arguments are "ex", "ey", "ez" for the vector components or "n" for the norm of the electric field.
+The list with options and defaults is shown with the -h option. A default value of 100 is used for the binning, but this can be changed.
+In a 3D mesh, the plane to be plotted must be identified by using the option -p with argument *xy*, *yz* or *zx*, defaulting to *yz*.
+The data to be ploted can be selected with the -d option, the arguments are *ex*, *ey*, *ez* for the vector components or the default value *n* for the norm of the electric field.
 
 
 #### Octree
--- tools/tcad_dfise_converter/data/mesh_plotter.cpp
+++ tools/tcad_dfise_converter/data/mesh_plotter.cpp
@@ -9,6 +9,7 @@
 #include "TStyle.h"
 
 int main(int argc, char** argv) {
+    // Set ROOT params
     gStyle->SetOptStat(0);
     gStyle->SetNumberContours(999);
 
@@ -19,7 +20,9 @@ int main(int argc, char** argv) {
         return_code = 1;
     }
 
-    std::string file_prefix = "example_pixel";
+    // Read parameters
+    std::string file_name;
+    std::string output_file_name = "efield.png";
     std::string plane = "yz";
     std::string data = "n";
     int slice_index = 0;
@@ -27,55 +30,61 @@ int main(int argc, char** argv) {
     int xdiv = 100;
     int ydiv = 100;
     int zdiv = 100;
-
-    std::stringstream file_name;
-
     for(int i = 1; i < argc; i++) {
         if(strcmp(argv[i], "-h") == 0) {
             print_help = true;
         } else if(strcmp(argv[i], "-f") == 0 && (i + 1 < argc)) {
-            file_prefix = std::string(argv[++i]);
+            file_name = std::string(argv[++i]);
+        } else if(strcmp(argv[i], "-o") == 0 && (i + 1 < argc)) {
+            output_file_name = std::string(argv[++i]);
         } else if(strcmp(argv[i], "-p") == 0 && (i + 1 < argc)) {
             plane = std::string(argv[++i]);
         } else if(strcmp(argv[i], "-d") == 0 && (i + 1 < argc)) {
             data = std::string(argv[++i]);
         } else if(strcmp(argv[i], "-c") == 0 && (i + 1 < argc)) {
-            slice_cut = static_cast<int>(strtod(argv[++i], nullptr));
+            slice_cut = std::atoi(argv[++i]);
         } else if(strcmp(argv[i], "-x") == 0 && (i + 1 < argc)) {
-            xdiv = static_cast<int>(strtod(argv[++i], nullptr));
+            xdiv = std::atoi(argv[++i]);
         } else if(strcmp(argv[i], "-y") == 0 && (i + 1 < argc)) {
-            ydiv = static_cast<int>(strtod(argv[++i], nullptr));
+            ydiv = std::atoi(argv[++i]);
         } else if(strcmp(argv[i], "-z") == 0 && (i + 1 < argc)) {
-            zdiv = static_cast<int>(strtod(argv[++i], nullptr));
+            zdiv = std::atoi(argv[++i]);
         } else {
             std::cout << "Unrecognized command line argument \"" << argv[i] << std::endl;
+            print_help = true;
         }
+    }
+
+    if(file_name.empty()) {
+        print_help = true;
     }
 
     if(print_help) {
         std::cerr << "Usage: ./tcad_dfise_reader -f <data_file_prefix> [<options>]" << std::endl;
-        std::cout << "\t -f <file_prefix>       DF-ISE files prefix" << std::endl;
-        std::cout << "\t -p <plane>             plane to be ploted. xy, yz or zx" << std::endl;
-        std::cout << "\t -d <data>              data to be read. Check read me file" << std::endl;
-        std::cout << "\t -c <cut>               define projection height" << std::endl;
-        std::cout << "\t -x <mesh x_pitch>      new regular mesh X pitch" << std::endl;
-        std::cout << "\t -y <mesh_y_pitch>      new regular mesh Y pitch" << std::endl;
-        std::cout << "\t -z <mesh_z_pitch>      new regular mesh Z pitch" << std::endl;
+        std::cout << "\t -f <file_name>         init file name" << std::endl;
+        std::cout << "\t -o <output_file_name>  optional name of the file to output (default is efield.png)" << std::endl;
+        std::cout << "\t -p <plane>             optional plane to be ploted. xy, yz or zx (default is xy)" << std::endl;
+        std::cout << "\t -d <data>              optional data to be read (default is n). See README file." << std::endl;
+        std::cout << "\t -c <cut>               optional projection height index (default is 1)" << std::endl;
+        std::cout << "\t -x <mesh x_pitch>      optional plot regular mesh X binning (default is 100)" << std::endl;
+        std::cout << "\t -y <mesh_y_pitch>      optional plot regular mesh Y binning (default is 100)" << std::endl;
+        std::cout << "\t -z <mesh_z_pitch>      optional plot regular mesh Z binning (default is 100)" << std::endl;
         return return_code;
     }
 
-    file_name << file_prefix << "_" << xdiv << "x" << ydiv << "x" << zdiv << ".init";
-    std::cout << "Reading file: " << file_name.str();
+    // Read file
+    std::cout << "Reading file: " << file_name;
 
     std::ifstream input_file;
-    input_file.open((file_name.str()).c_str());
+    input_file.open(file_name);
     if(input_file.is_open() != false) {
-        std::cout << "	OK" << std::endl;
+        std::cout << "  OK" << std::endl;
     } else {
-        std::cout << "	FAILD" << std::endl;
-        return -1;
+        std::cout << "  FAILED" << std::endl;
+        return 1;
     }
 
+    // Find plotting indices
     int x_bin = 0;
     int y_bin = 0;
     int x_bin_index = 0;
@@ -113,6 +122,7 @@ int main(int argc, char** argv) {
         data_index = 5;
     }
 
+    // Create and fill histogram
     auto efield_map = new TH2D("Electric Field", "Electric Field", x_bin, 0, x_bin, y_bin, 0, y_bin);
     auto c1 = new TCanvas();
 
@@ -144,6 +154,6 @@ int main(int argc, char** argv) {
     }
     c1->cd();
     efield_map->Draw("colz");
-    c1->SaveAs("efield_map.png");
-    return 1;
+    c1->SaveAs(output_file_name.c_str());
+    return 0;
 }
