d5924ea81692c9cc288231ed3d88dc12c5c0a7e3
Simon Spannagel <simon.spannagel@cern.ch>
2017-08-08 10:29:10 +0200

    Add ThreadPool for parallelizing module execution

---
+++ src/core/module/ThreadPool.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file
+ * @brief Definition of thread pool used for module multithreading
+ * @copyright MIT License
+ */
+
+#include "ThreadPool.hpp"
+
+#include "Module.hpp"
+
+using namespace allpix;
+
+/**
+ * The threads are created in an exception-safe way and all will be destroyed when creating them fails
+ */
+ThreadPool::ThreadPool(unsigned int num_threads, std::vector<Module*> modules) {
+    // Create threads
+    try {
+        for(unsigned int i = 0u; i < num_threads; ++i) {
+            threads_.emplace_back(&ThreadPool::worker, this);
+        }
+    } catch(...) {
+        destroy();
+        throw;
+    }
+
+    // Add module module queues
+    for(auto& module : modules) {
+        // Default constructs a queue for all modules
+        // NOTE: this is the only valid method due to SafeTask not being movable
+        task_queues_[module];
+    }
+    run_cnt_ = 0;
+}
+
+void ThreadPool::submit_module_function(std::function<void()> module_function) {
+    module_queue_.push(std::make_unique<std::packaged_task<void()>>(std::move(module_function)));
+    all_queue_.push(&module_queue_);
+}
+
+ThreadPool::~ThreadPool() {
+    destroy();
+}
+
+/**
+ * @warning This function does not wait for the all the running tasks to finish
+ *
+ * Should always be run by the thread spawning tasks, to ensure the task can be completed when there are no other threads
+ * available to execute them.
+ */
+bool ThreadPool::execute(Module* module) {
+    // Run tasks until the queue is empty
+    Task task{nullptr};
+    while(task_queues_.at(module).pop(task, false)) {
+        (*task)();
+    }
+    return task_queues_.at(module).isValid();
+}
+
+/**
+ * Run by the \ref ModuleManager to ensure all tasks and modules are completed before moving to the next instantiations.
+ * Besides waiting for the queue to empty this will also wait for all the tasks to be completed.
+ */
+bool ThreadPool::execute_all() {
+    while(true) {
+        // Run tasks until the queue is empty
+        SafeQueue<Task>* queue_ptr;
+        while(all_queue_.pop(queue_ptr, false)) {
+            Task task{nullptr};
+            if(queue_ptr->pop(task, false)) {
+                (*task)();
+            }
+        }
+
+        // Wait for the threads to complete their task, continue helping if a new task was pushed
+        std::unique_lock<std::mutex> lock{run_mutex_};
+        run_condition_.wait(lock, [this]() { return !all_queue_.empty() || run_cnt_ == 0; });
+
+        // Only stop when both the queue is empty and the run count is zero
+        if(all_queue_.empty() && run_cnt_ == 0) {
+            break;
+        }
+    }
+
+    return all_queue_.isValid();
+}
+
+void ThreadPool::worker() {
+    // FIXME: use proper initialization
+    // ALERT : temporarily set default log level to INFO
+    Log::setReportingLevel(LogLevel::INFO);
+
+    // Safe lambda to increase the atomic run count
+    auto increase_run_cnt_func = [this]() { ++run_cnt_; };
+
+    // Continue running until the thread pool is finished
+    while(!done_) {
+        SafeQueue<Task>* queue_ptr;
+        Task task{nullptr};
+        if(all_queue_.pop(queue_ptr) && queue_ptr->pop(task, false, increase_run_cnt_func)) {
+            (*task)();
+
+            // Propagate that the task has been finished
+            std::lock_guard<std::mutex> lock{run_mutex_};
+            --run_cnt_;
+            run_condition_.notify_all();
+        }
+    }
+}
+
+void ThreadPool::destroy() {
+    done_ = true;
+
+    all_queue_.invalidate();
+    module_queue_.invalidate();
+    for(auto& queue : task_queues_) {
+        queue.second.invalidate();
+    }
+
+    for(auto& thread : threads_) {
+        if(thread.joinable()) {
+            thread.join();
+        }
+    }
+}
---
+++ src/core/module/ThreadPool.tpp
@@ -0,0 +1,70 @@
+namespace allpix {
+    template <typename Func, typename... Args> auto ThreadPool::submit(Module* module, Func&& func, Args&&... args) {
+        // Bind the arguments to the tasks
+        auto bound_task = std::bind(std::forward<Func>(func), std::forward<Args>(args)...);
+
+        // Construct packaged task with correct return type
+        using PackagedTask = std::packaged_task<decltype(bound_task())()>;
+        PackagedTask task(bound_task);
+
+        // Get future and wrapper to add to vector
+        auto future = task.get_future();
+        auto task_function = [task = std::move(task)]() mutable { task(); };
+        task_queues_.at(module).push(std::make_unique<std::packaged_task<void()>>(std::move(task_function)));
+        all_queue_.push(&task_queues_.at(module));
+        return future;
+    }
+
+    template <typename T> ThreadPool::SafeQueue<T>::~SafeQueue() { invalidate(); }
+
+    /*
+    * Block until a value is available if the wait parameter is set to true. The wait exits when the queue is invalidated.
+    */
+    template <typename T> bool ThreadPool::SafeQueue<T>::pop(T& out, bool wait, const std::function<void()>& func) {
+        // Lock the mutex
+        std::unique_lock<std::mutex> lock{mutex_};
+        if(wait) {
+            // Wait for new item in the queue (unlocks the mutex while waiting)
+            condition_.wait(lock, [this]() { return !queue_.empty() || !valid_; });
+        }
+        // Check for empty and valid queue
+        if(queue_.empty() || !valid_) {
+            return false;
+        }
+
+        // Pop the queue and optionally execute the mutex protected function
+        out = std::move(queue_.front());
+        queue_.pop();
+        if(func != nullptr) {
+            func();
+        }
+        return true;
+    }
+
+    template <typename T> void ThreadPool::SafeQueue<T>::push(T value) {
+        std::lock_guard<std::mutex> lock{mutex_};
+        queue_.push(std::move(value));
+        condition_.notify_one();
+    }
+
+    template <typename T> bool ThreadPool::SafeQueue<T>::isValid() const {
+        std::lock_guard<std::mutex> lock{mutex_};
+        return valid_;
+    }
+
+    template <typename T> bool ThreadPool::SafeQueue<T>::empty() const {
+        std::lock_guard<std::mutex> lock{mutex_};
+        return queue_.empty();
+    }
+
+    /*
+    * Used to ensure no conditions are being waited for in pop when a thread or the application is trying to exit. The queue
+    * is invalid after calling this method and it is an error to continue using a queue after this method has been called.
+    */
+    template <typename T> void ThreadPool::SafeQueue<T>::invalidate() {
+        std::lock_guard<std::mutex> lock{mutex_};
+        std::queue<T>().swap(queue_);
+        valid_ = false;
+        condition_.notify_all();
+    }
+}
