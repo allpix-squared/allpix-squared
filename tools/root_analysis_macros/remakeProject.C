0a924f42689ea380566ee06c93a1dbbb05804b58
Koen Wolters <koen.wolters@cern.ch>
2017-08-10 13:02:19 +0200

    update ROOT analysis README

--- tools/root_analysis_macros/remakeProject.C
+++ tools/root_analysis_macros/remakeProject.C
@@ -3,6 +3,6 @@
 /**
  * Recreate project source files from data stored in data file
  */
-void remakeProject(TFile* file, std::string file_name) {
-    file->MakeProject(file_name.c_str(), "*", "RECREATE+");
+void remakeProject(TFile* file, std::string dir_name) {
+    file->MakeProject(dir_name.c_str(), "*", "RECREATE+");
 }
