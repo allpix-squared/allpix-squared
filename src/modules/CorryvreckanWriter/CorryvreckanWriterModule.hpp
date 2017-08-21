455ffeff1fb4a9e364b0543ea10493b9db69575d
Koen Wolters <koen.wolters@cern.ch>
2017-08-21 15:11:39 +0200

    update license information

--- src/modules/CorryvreckanWriter/CorryvreckanWriterModule.hpp
+++ src/modules/CorryvreckanWriter/CorryvreckanWriterModule.hpp
@@ -1,11 +1,10 @@
 /**
  * @file
  * @brief Definition of CorryvreckanWriter module
- * @copyright MIT License
- *
- * Contains minimal dummy module to use as a start for the development of your own module
- *
- * Refer to the User's Manual for more details.
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
 #include <string>
--- src/modules/DepositionGeant4/SensitiveDetectorActionG4.cpp
+++ src/modules/DepositionGeant4/SensitiveDetectorActionG4.cpp
@@ -2,7 +2,10 @@
  * @file
  * @brief Implements the handling of the sensitive device
  * @remarks Based on code from John Idarraga
- * @copyright MIT License
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
 #include "SensitiveDetectorActionG4.hpp"
