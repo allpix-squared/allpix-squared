83ef7d821795d22794051cb36c839580b58f809e
Koen Wolters <koen.wolters@cern.ch>
2017-08-28 13:19:20 +0200

    output running times of module in order of execution

--- src/core/module/ModuleManager.cpp
+++ src/core/module/ModuleManager.cpp
@@ -736,8 +736,8 @@ void ModuleManager::finalize() {
     LOG(STATUS) << "Executed " << modules_.size() << " instantiations in " << seconds_to_time(total_time_) << ", spending "
                 << std::round((100 * slowest_time) / std::max(1.0l, total_time_)) << "% of time in slowest instantiation "
                 << slowest_module;
-    for(auto& module_time : module_execution_time_) {
-        LOG(INFO) << " Module " << module_time.first->getUniqueName() << " took " << module_time.second << " seconds";
+    for(auto& module : modules_) {
+        LOG(INFO) << " Module " << module->getUniqueName() << " took " << module_execution_time_[module.get()] << " seconds";
     }
     long double processing_time = 0;
     if(global_config_.get<unsigned int>("number_of_events") > 0) {
