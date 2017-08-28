d4990e508f28b34d19dc844ec9718074557a29a1
Simon Spannagel <simon.spannagel@cern.ch>
2017-08-28 15:42:06 +0200

    DefaultDigitizer: add ADC simulation

--- src/modules/DefaultDigitizer/DefaultDigitizerModule.cpp
+++ src/modules/DefaultDigitizer/DefaultDigitizerModule.cpp
@@ -34,7 +34,12 @@ DefaultDigitizerModule::DefaultDigitizerModule(Configuration config,
     config_.setDefault<int>("electronics_noise", Units::get(110, "e"));
     config_.setDefault<int>("threshold", Units::get(600, "e"));
     config_.setDefault<int>("threshold_smearing", Units::get(30, "e"));
+
+    config_.setDefault<int>("adc_resolution", 0);
     config_.setDefault<int>("adc_smearing", Units::get(300, "e"));
+    config_.setDefault<int>("adc_offset", Units::get(0, "e"));
+    config_.setDefault<double>("adc_slope", Units::get(10, "e"));
+
     config_.setDefault<bool>("output_plots", false);
 }
 
@@ -48,6 +53,12 @@ void DefaultDigitizerModule::init() {
         h_thr = new TH1D("threshold", "applied threshold; threshold [ke];events", 100, 0, 10);
         h_pxq_thr = new TH1D("pixelcharge_threshold_", "pixel charge above threshold;pixel charge [ke];pixels", 100, 0, 10);
         h_pxq_adc = new TH1D("pixelcharge_adc", "pixel charge after ADC;pixel charge [ke];pixels", 100, 0, 10);
+    }
+
+    // Conversion to ADC units requested:
+    if(config_.get<int>("adc_resolution") > 0) {
+        LOG(INFO) << "Converting charge to ADC units, ADC resolution: " << config_.get<int>("adc_resolution")
+                  << "bit, max. value " << ((1 << config_.get<int>("adc_resolution")) - 1);
     }
 }
 
@@ -95,19 +106,26 @@ void DefaultDigitizerModule::run(unsigned int) {
             h_pxq_thr->Fill(charge / 1e3);
         }
 
-        // Add ADC smearing:
-        std::normal_distribution<double> adc_smearing(0, config_.get<unsigned int>("adc_smearing"));
-        charge += adc_smearing(random_generator_);
-        if(config_.get<bool>("output_plots")) {
-            h_pxq_adc->Fill(charge / 1e3);
+        // Simulate ADC if resolution set to more than 0bit
+        if(config_.get<int>("adc_resolution") > 0) {
+            // Add ADC smearing:
+            std::normal_distribution<double> adc_smearing(0, config_.get<unsigned int>("adc_smearing"));
+            charge += adc_smearing(random_generator_);
+            if(config_.get<bool>("output_plots")) {
+                h_pxq_adc->Fill(charge / 1e3);
+            }
+            LOG(DEBUG) << "Smeared for simulating limited ADC sensitivity: " << Units::display(charge, "e");
+
+            // Convert to ADC units and precision:
+            charge = std::max(
+                std::min(static_cast<int>(config_.get<int>("adc_offset") + charge / config_.get<double>("adc_slope")),
+                         (1 << config_.get<int>("adc_resolution")) - 1),
+                0);
+            LOG(DEBUG) << "Charge converted to ADC units: " << charge;
         }
 
         // Add the hit to the hitmap
         hits.emplace_back(pixel, 0, charge, &pixel_charge);
-
-        // FIXME Simulate analog / digital cross talk
-        // double crosstalk_neigubor_row = 0.00;
-        // double crosstalk_neigubor_column = 0.00;
     }
 
     // Output summary and update statistics
