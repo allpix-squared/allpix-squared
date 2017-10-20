31a67a2e11a3096abbc830ef24c4449be5c411db
Koen Wolters <koen.wolters@cern.ch>
2017-10-20 22:40:46 +0200

    handle single quotation marks in from_string converter

--- src/core/utils/string.h
+++ src/core/utils/string.h
@@ -139,9 +139,9 @@ namespace allpix {
      */
     inline std::string from_string_impl(std::string str, type_tag<std::string>) {
         str = trim(str);
-        // If there are "" then we should take the whole string (FIXME: '' should also be supported)
-        if(!str.empty() && str[0] == '\"') {
-            if(str.find('\"', 1) != str.size() - 1) {
+        // If there are "" then we should take the whole string
+        if(!str.empty() && (str.front() == '\"' || str.front() == '\'')) {
+            if(str.find(str.front(), 1) != str.size() - 1) {
                 throw std::invalid_argument("remaining data at end");
             }
             return str.substr(1, str.size() - 2);
