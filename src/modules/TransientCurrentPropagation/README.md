50be5a6e91d512126265479a03063a758a41212c
Simon Spannagel <simon.spannagel@cern.ch>
2017-12-12 11:12:46 +0100

    Add skeleton for TransientCurrentPropagation module

---
+++ src/modules/TransientCurrentPropagation/README.md
@@ -0,0 +1,14 @@
+## TransientCurrentPropagation
+**Maintainer**: Simon Spannagel (simon.spannagel@cern.ch)
+**Status**: Immature
+**Input**: DepositedCharge
+**Output**: PixelPulse
+
+#### Description
+*Short description of this module*
+
+#### Parameters
+* `param`: *explanation with optional default*
+
+#### Usage
+*Example how to use this module*
---
+++ src/modules/TransientCurrentPropagation/TransientCurrentPropagationModule.hpp
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * @brief Definition of [TransientCurrentPropagation] module
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ *
+ * Contains minimal dummy module to use as a start for the development of your own module
+ *
+ * Refer to the User's Manual for more details.
+ */
+
+#include <string>
+
+#include <Math/Point3D.h>
+
+#include "core/config/Configuration.hpp"
+#include "core/geometry/DetectorModel.hpp"
+#include "core/messenger/Messenger.hpp"
+#include "core/module/Module.hpp"
+
+#include "objects/DepositedCharge.hpp"
+#include "objects/PixelPulse.hpp"
+#include "tools/ROOT.h"
+
+namespace allpix {
+    /**
+     * @ingroup Modules
+     * @brief Module to do function
+     *
+     * More detailed explanation of module
+     */
+    class TransientCurrentPropagationModule : public Module {
+    public:
+        /**
+         * @brief Constructor for this detector-specific module
+         * @param config Configuration object for this module as retrieved from the steering file
+         * @param messenger Pointer to the messenger object to allow binding to messages on the bus
+         * @param detector Pointer to the detector for this module instance
+         */
+        TransientCurrentPropagationModule(Configuration config, Messenger* messenger, std::shared_ptr<Detector> detector);
+
+        /**
+         * @brief [Initialise this module]
+         */
+        void init() override;
+
+        /**
+         * @brief [Run the function of this module]
+         */
+        void run(unsigned int) override;
+
+    private:
+        // General module members
+        std::shared_ptr<const Detector> detector_;
+        Messenger* messenger_;
+        std::shared_ptr<DetectorModel> model_;
+        std::shared_ptr<DepositedChargeMessage> deposits_message_;
+
+        /**
+         * @brief Propagate a single set of charges through the sensor
+         * @param pos Position of the deposit in the sensor
+         * @param type Type of the carrier to propagate
+         * @return Pair of the point where the deposit ended after propagation and the time the propagation took
+         */
+        void propagate(const ROOT::Math::XYZPoint& pos,
+                       const CarrierType& type,
+                       const unsigned int charge,
+                       std::map<Pixel::Index, PixelPulse>& pixel_map);
+
+        // Random generator for this module
+        std::mt19937_64 random_generator_;
+
+        // Local copies of configuration parameters to avoid costly lookup:
+        double temperature_{}, timestep_{}, integration_time_{}, target_spatial_precision_{}, output_plots_step_{};
+
+        // Precalculated values for electron and hole mobility
+        double electron_Vm_;
+        double electron_Ec_;
+        double electron_Beta_;
+        double hole_Vm_;
+        double hole_Ec_;
+        double hole_Beta_;
+
+        // Precalculated value for Boltzmann constant:
+        double boltzmann_kT_;
+    };
+} // namespace allpix
