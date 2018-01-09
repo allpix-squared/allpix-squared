a517607c1ca0eeb9d84b438a5116bacaa71573a3
Simon Spannagel <simon.spannagel@cern.ch>
2018-01-09 15:08:12 +0000

    CPack: remove DEB, remove TargetArch

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -3,7 +3,7 @@ SET(CPACK_PACKAGE_NAME "allpix-squared")
 SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Generic Pixel Detector Simulation Framework")
 SET(CPACK_PACKAGE_VENDOR "The Allpix Squared Authors")
 SET(CPACK_PACKAGE_CONTACT "The Allpix Squared Authors <allpix.squared@cern.ch>")
-SET(CPACK_PACKAGE_ICON "doc/logo_small.png") # Doesn't work
+SET(CPACK_PACKAGE_ICON "doc/logo_small.png")
 SET(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE.md")
 
 # Set version dervied from project version:
@@ -15,10 +15,6 @@ ELSE()
     SET(CPACK_PACKAGE_VERSION_PATCH "${PROJECT_VERSION_PATCH}")
 ENDIF()
 
-# Debian-specific packaging configuration
-SET(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://cern.ch/allpix-squared/")
-# SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE )
-
 # Configure the targets and components to include
-SET(CPACK_GENERATOR "DEB;TGZ")
-SET(CPACK_COMPONENTS_ALL application modules models tools)
+SET(CPACK_GENERATOR "TGZ")
+SET(CPACK_COMPONENTS_ALL application modules)
