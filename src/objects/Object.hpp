211ca2618119ca33d24012356e91e838e9e701be
Simon Spannagel <simon.spannagel@cern.ch>
2018-02-06 14:47:58 +0100

    Object: override ROOT's Print() function to use out nicer print(ostream) version

--- src/objects/Object.hpp
+++ src/objects/Object.hpp
@@ -71,7 +71,18 @@ namespace allpix {
          * @param out Stream to print to
          */
         virtual void print(std::ostream& out) const { out << "<unknown object>"; };
+
+        /**
+         * @brief Override function for the ROOT Print() class
+         * @warning Should not be used inside the framework but might assist in inspecting ROOT files with these objects.
+         * @param Unused ROOT options
+         */
+        void Print(Option_t*) const override {
+            print(std::cout);
+            std::cout << std::endl;
+        }
     };
+
 } // namespace allpix
 
 #endif
