1aa211e00df903de5751b7f0277357b18e901aa6
Simon Spannagel <simon.spannagel@cern.ch>
2018-02-08 15:22:15 +0100

    Print build flavor
    (cherry picked from commit 0a0522b72c03e0602f8935212e7c0a60c5196b17)

--- .gitlab-ci.d/gitlab_deploy.sh
+++ .gitlab-ci.d/gitlab_deploy.sh
@@ -14,6 +14,7 @@ if [[ $clicdp_status == *"(stratum0 / local)"* ]]; then
   # Extract artifact tars
   echo "Extract the artifact tarballs"
   for FLAVOUR in "x86_64-centos7-gcc7-opt" "x86_64-slc6-gcc7-opt"; do
+    echo " - $FLAVOUR"
     mkdir -p /home/cvclicdp/release/$2/$FLAVOUR/
     tar --strip-components=1 -xf $1/allpix-squared-latest_$FLAVOUR.tar.gz -C /home/cvclicdp/release/$2/$FLAVOUR/
   done
