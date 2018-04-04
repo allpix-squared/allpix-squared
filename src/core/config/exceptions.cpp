6101d355970671d20cc24c07d32b1e2a7b583905
Koen Wolters <koen.wolters@cern.ch>
2018-04-04 12:39:04 +0200

    use global instead of empty section in exceptions

--- src/core/config/exceptions.cpp
+++ src/core/config/exceptions.cpp
@@ -16,7 +16,7 @@ using namespace allpix;
 InvalidValueError::InvalidValueError(const Configuration& config, const std::string& key, const std::string& reason) {
     std::string section_str = "in section '" + config.getName() + "'";
     if(config.getName().empty()) {
-        section_str = "in empty section";
+        section_str = "in global section";
     }
     error_message_ = "Value " + config.getText(key) + " of key '" + key + "' " + section_str + " is not valid";
     if(!reason.empty()) {
@@ -29,7 +29,7 @@ InvalidCombinationError::InvalidCombinationError(const Configuration& config,
                                                  const std::string& reason) {
     std::string section_str = "in section '" + config.getName() + "'";
     if(config.getName().empty()) {
-        section_str = "in empty section";
+        section_str = "in global section";
     }
     error_message_ = "Combination of keys ";
     for(auto& key : keys) {
