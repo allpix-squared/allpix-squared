e62cf36c1c96c0b0ff6680150d65887afbcb2e7d
Paul Schuetze <paul.schuetze@cern.ch>
2018-04-05 15:16:37 +0200

    Add unique module for reading and applying a magnetic field

---
+++ src/modules/MagneticFieldReader/MagneticFieldReaderModule.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * @brief Implementation of module to define magnetic fields
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#include "MagneticFieldReaderModule.hpp"
+
+#include <fstream>
+#include <limits>
+#include <memory>
+#include <new>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+#include <Math/Point3D.h>
+#include <Math/Vector3D.h>
+#include <TH2F.h>
+
+#include "core/config/exceptions.h"
+#include "core/geometry/DetectorModel.hpp"
+#include "core/utils/log.h"
+#include "core/utils/unit.h"
+
+using namespace allpix;
+
+MagneticFieldReaderModule::MagneticFieldReaderModule(Configuration config, Messenger*, GeometryManager* geoManager)
+    : Module(std::move(config)), geometryManager_(geoManager) {}
+
+void MagneticFieldReaderModule::init() {
+    MagneticFieldType type = MagneticFieldType::CONSTANT;
+
+    // Check field strength
+    auto field_model = config_.get<std::string>("model");
+
+    // Calculate the field depending on the configuration
+    if(field_model == "constant") {
+        LOG(TRACE) << "Adding constant magnetic field";
+        type = MagneticFieldType::CONSTANT;
+
+        auto b_field = config_.get<ROOT::Math::XYZVector>("magnetic_field", ROOT::Math::XYZVector());
+
+        LOG(INFO) << "Set constant magnetic field: (" << Units::display(b_field.x(), {"T", "mT"}) << ","
+                  << Units::display(b_field.y(), {"T", "mT"}) << "," << Units::display(b_field.z(), {"T", "mT"}) << ")";
+        MagneticFieldFunction function = [b_field](const ROOT::Math::XYZPoint&) { return b_field; };
+
+        geometryManager_->setMagneticFieldFunction(function, type);
+        std::vector<std::shared_ptr<Detector>> detectors = geometryManager_->getDetectors();
+        for(auto& detector : detectors) {
+            detector->setMagneticField(geometryManager_->getMagneticFieldAtPos(detector->getPosition()));
+        }
+    } else {
+        throw InvalidValueError(config_, "model", "model can currently only be 'constant'");
+    }
+}
