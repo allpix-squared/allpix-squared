8eb08e8c020c91f02e6e02436b143f372ceb04b1
tbisanz <tobias.bisanz@phys.uni-goettingen.de>
2018-04-09 13:49:36 +0200

    LINT formatting for UserHookSetUniqueTrackID.cpp

--- src/modules/DepositionGeant4/UserHookSetUniqueTrackID.cpp
+++ src/modules/DepositionGeant4/UserHookSetUniqueTrackID.cpp
@@ -4,6 +4,6 @@
 using namespace allpix;
 
 void UserHookSetUniqueTrackID::PreUserTrackingAction(const G4Track* aTrack) {
-    G4Track* theTrack = const_cast<G4Track*>(aTrack);
+    auto theTrack = const_cast<G4Track*>(aTrack); // NOLINT
     theTrack->SetUserInformation(new AllpixG4TrackInfo(aTrack->GetTrackID()));
 }
