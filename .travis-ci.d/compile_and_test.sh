df140f7e8dca227d0820097d251e8826bb12ec4f
Simon Spannagel <simon.spannagel@cern.ch>
2018-04-25 15:25:02 +0200

    Travis-CI: re-add SFT CVMFS repo

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -4,5 +4,4 @@ mkdir build
 cd build
 source ../.gitlab-ci.d/init_x86_64.sh
 cmake -DBUILD_ALL_MODULES=ON -DGeant4_DIR=$G4LIB -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
-export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 ninja
