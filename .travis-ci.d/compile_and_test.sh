c71efd3a8d2fabdbb8eea15184ef5231bc1f514d
Simon Spannagel <simon.spannagel@cern.ch>
2018-04-25 15:50:04 +0200

    Travis-CI: enable clang-format and the test suite

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -4,4 +4,7 @@ mkdir build
 cd build
 source ../.gitlab-ci.d/init_x86_64.sh
 cmake -GNinja -DBUILD_ALL_MODULES=ON -DGeant4_DIR=$G4LIB -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
-ninja
+ninja && \
+ninja install && \
+ninja check-format && \
+ctest --output-on-failure -j4
