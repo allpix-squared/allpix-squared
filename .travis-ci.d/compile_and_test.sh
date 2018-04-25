4f3924d0d7192d120b501741b4e0cf59474039a5
Simon Spannagel <simon.spannagel@cern.ch>
2018-04-25 15:31:48 +0200

    Travis-CI: enable ninja as build tool

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -3,5 +3,5 @@ cd /Package
 mkdir build
 cd build
 source ../.gitlab-ci.d/init_x86_64.sh
-cmake -DBUILD_ALL_MODULES=ON -DGeant4_DIR=$G4LIB -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
+cmake -GNinja -DBUILD_ALL_MODULES=ON -DGeant4_DIR=$G4LIB -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
 ninja
