72d52294ff9f992da3e10d9bc8e78c3643b75672
Simon Spannagel <simon.spannagel@cern.ch>
2018-05-03 15:56:28 +0200

    Remove ClassImp as done for other classes in !139

--- src/modules/CorryvreckanWriter/corryvreckan/Object.cpp
+++ src/modules/CorryvreckanWriter/corryvreckan/Object.cpp
@@ -10,5 +10,3 @@ Object::Object(std::string detectorID) : m_detectorID(detectorID), m_timestamp(0
 Object::Object(double timestamp) : m_detectorID(), m_timestamp(timestamp) {}
 Object::Object(std::string detectorID, double timestamp) : m_detectorID(detectorID), m_timestamp(timestamp) {}
 Object::~Object() {}
-
-ClassImp(Object)
