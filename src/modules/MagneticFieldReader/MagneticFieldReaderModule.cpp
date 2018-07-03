68ff2d1aed124a914bf6b26d4cddd46ec1ab79dd
Katharina Dort <katharina.dort@cern.ch>
2018-07-03 14:21:27 +0200

    Fixed: magnetic field in detector for arbitrary detector orientation.
    (cherry picked from commit 70b54815589f38d474f49596bcf89f8a71df5048)

--- src/modules/MagneticFieldReader/MagneticFieldReaderModule.cpp
+++ src/modules/MagneticFieldReader/MagneticFieldReaderModule.cpp
@@ -47,14 +47,19 @@ void MagneticFieldReaderModule::init() {
         MagneticFieldFunction function = [b_field](const ROOT::Math::XYZPoint&) { return b_field; };
 
         geometryManager_->setMagneticFieldFunction(function, type);
-        std::vector<std::shared_ptr<Detector>> detectors = geometryManager_->getDetectors();
+        auto detectors = geometryManager_->getDetectors();
         for(auto& detector : detectors) {
             // TODO the magnetic field is calculated once for the center position of the detector. This could be extended to
             // a function enabling a gradient in the magnetic field inside the sensor
-            detector->setMagneticField(geometryManager_->getMagneticField(detector->getPosition()));
+	  detector->setMagneticField(detector->getOrientation() *                 		   			    geometryManager_->getMagneticField(detector->getPosition()));
+	  LOG(DEBUG) << "Magnetic field in detector " 
+		     << detector->getName() << ": " 
+		     << Units::display(detector->getMagneticField(), {"T", "mT"});
         }
         LOG(INFO) << "Set constant magnetic field: " << Units::display(b_field, {"T", "mT"});
     } else {
         throw InvalidValueError(config_, "model", "model can currently only be 'constant'");
     }
 }
+
+
