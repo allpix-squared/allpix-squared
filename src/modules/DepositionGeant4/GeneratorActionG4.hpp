904fb3a89e9e51e1f702c599429dc20a9c465669
Simon Spannagel <simon.spannagel@cern.ch>
2018-07-15 15:26:31 +0200

    Deposition: remove leftover function definition

--- src/modules/DepositionGeant4/GeneratorActionG4.hpp
+++ src/modules/DepositionGeant4/GeneratorActionG4.hpp
@@ -40,7 +40,6 @@ namespace allpix {
 
     private:
         std::unique_ptr<G4GeneralParticleSource> particle_source_;
-        void add_multiple_decay(std::string, const double*, const double*, int);
     };
 } // namespace allpix
 
