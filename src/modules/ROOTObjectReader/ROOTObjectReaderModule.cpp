eba549b47b0f03098e16d59d8bd6d7c03d3b1919
Katharina Dort <katharina.dort@cern.ch>
2018-09-04 10:53:10 +0200

    Instead of using two different if-statements to test if random seed core from config file is missing and if there is a mismatch, one if statement is used.
    (cherry picked from commit d30b0ed2b0939560aa6a7394aaa230444778226b)

--- src/modules/ROOTObjectReader/ROOTObjectReaderModule.cpp
+++ src/modules/ROOTObjectReader/ROOTObjectReaderModule.cpp
@@ -136,25 +136,19 @@ void ROOTObjectReaderModule::init() {
 
     std::string* str = nullptr;
     input_file_->GetObject("config/Allpix/random_seed_core", str);
-    uint64_t file_seed;
 
     if(str == nullptr) {
         // check if missing random seed core in config file should be ignored
         if(config_.get<bool>("ignore_seed_mismatch", false)) {
             LOG(WARNING) << "No random seed for core set in the input data file, cross-check with configured value - "
-                         << "this might lead to unexpected behavior. Random seed core from the input data is be used.";
-            file_seed = config_seed;
+                         << "this might lead to unexpected behavior. Random seed core from the input data is used.";
         } else {
             throw InvalidValueError(global_config,
                                     "random_seed_core",
                                     "no random seed for core set in the input data file, cross-check with configured value "
                                     "impossible - this might lead to unexpected behavior.");
         }
-    } else {
-        file_seed = allpix::from_string<uint64_t>(*str);
-    }
-
-    if(config_seed != file_seed) {
+    } else if(config_seed != allpix::from_string<uint64_t>(*str)) {
         // check if mismatch between random seed core in config and in input file should be ignored
         if(config_.get<bool>("ignore_seed_mismatch", false)) {
             LOG(WARNING) << "Mismatch between core random seed in configuration file and input data"
