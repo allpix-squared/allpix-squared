77cd0158b9dabcc4b905e768bdd6055247fa6b43
Simon Spannagel <simon.spannagel@cern.ch>
2018-11-07 22:06:26 +0100

    Units: we have to allow empty units if we want e.g. "/cm/s".

--- src/core/utils/unit.cpp
+++ src/core/utils/unit.cpp
@@ -50,7 +50,7 @@ void Units::add(std::string str, UnitType value) {
  */
 allpix::Units::UnitType Units::getSingle(std::string str) {
     if(allpix::trim(str).empty()) {
-        throw std::invalid_argument("empty unit is not defined");
+        return 1.;
     }
     // Do not distinguish between different case for units
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
