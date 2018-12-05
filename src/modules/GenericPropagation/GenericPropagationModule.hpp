fb42129f61826487857347de33d021c8fcb3cfb5
Paul Schuetze <paul.schuetze@cern.ch>
2018-12-05 15:28:35 +0100

    Change some more variable/option names from output_plots_* to output_linegraphs_* for a better consistency

--- src/modules/GenericPropagation/GenericPropagationModule.hpp
+++ src/modules/GenericPropagation/GenericPropagationModule.hpp
@@ -84,8 +84,8 @@ namespace allpix {
 
         // Local copies of configuration parameters to avoid costly lookup:
         double temperature_{}, timestep_min_{}, timestep_max_{}, timestep_start_{}, integration_time_{},
-            target_spatial_precision_{}, output_plots_step_{};
-        bool output_plots_{}, output_linegraphs_{}, output_animations_{}, output_plots_lines_at_implants_{};
+            target_spatial_precision_{}, output_linegraphs_step_{};
+        bool output_plots_{}, output_linegraphs_{}, output_animations_{}, output_linegraphs_lines_at_implants_{};
 
         // Precalculated values for electron and hole mobility
         double electron_Vm_;
