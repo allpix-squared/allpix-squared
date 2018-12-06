4e73596a2d8d224313e22d6074a0453ed8ae12ec
Simon Spannagel <simon.spannagel@cern.ch>
2018-12-06 13:53:35 +0100

    GenProp: Fix axis label of plot

--- src/modules/GenericPropagation/GenericPropagationModule.cpp
+++ src/modules/GenericPropagation/GenericPropagationModule.cpp
@@ -515,7 +515,7 @@ void GenericPropagationModule::init() {
 
     if(output_plots_) {
         step_length_histo_ = new TH1D("step_length_histo",
-                                      "Step length;length[um];integration steps",
+                                      "Step length;length [#mum];integration steps",
                                       100,
                                       0,
                                       static_cast<double>(Units::convert(0.25 * model_->getSensorSize().z(), "um")));
