74f30085c0dbb862cac86a9f79ddaadf01629385
Simon Spannagel <simon.spannagel@cern.ch>
2019-02-17 17:51:03 +0100

    New module: PulseTransfer, combines pulses instead of total charges

---
+++ src/modules/PulseTransfer/PulseTransferModule.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * @brief Implementation of PulseTransfer module
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#include "PulseTransferModule.hpp"
+#include "objects/PixelCharge.hpp"
+
+#include <string>
+#include <utility>
+
+#include <TAxis.h>
+#include <TGraph.h>
+
+#include "core/utils/log.h"
+
+using namespace allpix;
+
+PulseTransferModule::PulseTransferModule(Configuration& config, Messenger* messenger, std::shared_ptr<Detector> detector)
+    : Module(config, detector), detector_(detector), messenger_(messenger) {
+
+    config_.setDefault<bool>("output_pulsegraphs", false);
+    config_.setDefault<bool>("output_plots", config_.get<bool>("output_pulsegraphs"));
+
+    output_plots_ = config_.get<bool>("output_plots");
+    output_pulsegraphs_ = config_.get<bool>("output_pulsegraphs");
+
+    messenger_->bindSingle(this, &PulseTransferModule::message_, MsgFlags::REQUIRED);
+}
+
+void PulseTransferModule::run(unsigned int event_num) {
+
+    // Create map for all pixels
+    std::map<Pixel::Index, Pulse> pixel_map;
+
+    Pulse total_pulse;
+
+    for(const auto& prop : message_->getData()) {
+        auto pulses = prop.getPulses();
+        pixel_map =
+            std::accumulate(pulses.begin(), pulses.end(), pixel_map, [](std::map<Pixel::Index, Pulse>& m, const auto& p) {
+                return (m[p.first] += p.second, m);
+            });
+    }
+
+    // Create vector of pixel pulses to return for this detector
+    std::vector<PixelCharge> pixel_charges;
+    for(auto& pixel_index_pulse : pixel_map) {
+        total_pulse += pixel_index_pulse.second;
+
+        // Fill a graphs with the individual pixel pulses:
+        if(output_pulsegraphs_) {
+            auto index = pixel_index_pulse.first;
+            auto pulse = pixel_index_pulse.second.getPulse();
+            auto step = pixel_index_pulse.second.getBinning();
+
+            std::string name =
+                "pulse_px" + std::to_string(index.x()) + "-" + std::to_string(index.y()) + "_" + std::to_string(event_num);
+
+            // Generate x-axis:
+            std::vector<double> time(pulse.size());
+            std::generate(time.begin(), time.end(), [ n = 0.0, step ]() mutable { return n += step; });
+
+            auto pulse_graph = new TGraph(static_cast<int>(pulse.size()), &time[0], &pulse[0]);
+            pulse_graph->GetXaxis()->SetTitle("t [ns]");
+            pulse_graph->GetYaxis()->SetTitle("Q_{ind} [e]");
+            pulse_graph->SetTitle(("Induced charge in pixel (" + std::to_string(index.x()) + "," +
+                                   std::to_string(index.y()) +
+                                   "), Q_{tot} = " + std::to_string(pixel_index_pulse.second.getCharge()) + " e")
+                                      .c_str());
+            pulse_graph->Write(name.c_str());
+        }
+
+        // Store the pulse:
+        pixel_charges.emplace_back(detector_->getPixel(pixel_index_pulse.first), std::move(pixel_index_pulse.second));
+    }
+
+    // Create a new message with pixel pulses
+    auto pixel_charge_message = std::make_shared<PixelChargeMessage>(std::move(pixel_charges), detector_);
+
+    // Dispatch the message with pixel charges
+    messenger_->dispatchMessage(this, pixel_charge_message);
+
+    LOG(INFO) << "Total charge induced on all pixels: " << Units::display(total_pulse.getCharge(), "e");
+}
---
+++ src/modules/PulseTransfer/PulseTransferModule.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @brief Definition of PulseTransfer module
+ * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ *
+ * Contains minimal dummy module to use as a start for the development of your own module
+ *
+ * Refer to the User's Manual for more details.
+ */
+
+#include <string>
+
+#include "core/config/Configuration.hpp"
+#include "core/geometry/DetectorModel.hpp"
+#include "core/messenger/Messenger.hpp"
+#include "core/module/Module.hpp"
+
+#include "objects/PropagatedCharge.hpp"
+
+namespace allpix {
+    /**
+     * @ingroup Modules
+     * @brief Module to combine all charges induced at every pixel.
+     *
+     * This modules takes pulses created by the TransientPropagation module and compines the individual induced charges from
+     * each of the PropagatedCharges at every pixel into PixelCharges.
+     */
+    class PulseTransferModule : public Module {
+    public:
+        /**
+         * @brief Constructor for the PulseTransfer module
+         * @param config Configuration object for this module as retrieved from the steering file
+         * @param messenger Pointer to the messenger object to allow binding to messages on the bus
+         * @param detector Pointer to the detector for this module instance
+         */
+        PulseTransferModule(Configuration& config, Messenger* messenger, std::shared_ptr<Detector> detector);
+
+        /**
+         * @brief Combine pulses from propagated charges and transfer them to the pixels
+         */
+        void run(unsigned int) override;
+
+    private:
+        bool output_plots_{}, output_pulsegraphs_{};
+
+        // General module members
+        std::shared_ptr<Detector> detector_;
+        Messenger* messenger_;
+        std::shared_ptr<PropagatedChargeMessage> message_;
+    };
+} // namespace allpix
