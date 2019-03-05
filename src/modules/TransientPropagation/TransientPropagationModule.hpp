d4cbc3aff030390f719f7556add9883fb0188dc1
Simon Spannagel <simon.spannagel@cern.ch>
2019-03-05 16:44:24 +0100

    Update header information for new modules

--- src/modules/TransientPropagation/TransientPropagationModule.hpp
+++ src/modules/TransientPropagation/TransientPropagationModule.hpp
@@ -1,14 +1,10 @@
 /**
  * @file
  * @brief Definition of charge propagation module with transient behavior simulation
- * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
+ * @copyright Copyright (c) 2019 CERN and the Allpix Squared authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
- *
- * Contains minimal dummy module to use as a start for the development of your own module
- *
- * Refer to the User's Manual for more details.
  */
 
 #include <string>
--- src/modules/WeightingPotentialReader/WeightingPotentialReaderModule.hpp
+++ src/modules/WeightingPotentialReader/WeightingPotentialReaderModule.hpp
@@ -1,11 +1,9 @@
 /**
  * @file
  * @brief Definition of module to read weighting fields
- * @copyright Copyright (c) 2017 CERN and the Allpix Squared authors.
- * This software is distributed under the terms of the MIT License, copied
- * verbatim in the file "LICENSE.md".
- * In applying this license, CERN does not waive the privileges and immunities
- * granted to it by virtue of its status as an
+ * @copyright Copyright (c) 2019 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
