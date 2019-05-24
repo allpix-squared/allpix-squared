f465f4e503154256c256be19f51338733fe95d84
Koen van den Brandt <kbrandt@yuan.nikhef.nl>
2019-05-24 13:46:28 +0200

    Added a seperate module for the GDML output writer + rewritten the code a bit

---
+++ src/modules/GDMLOutputWriter/GDMLOutputWriterModule.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * @brief Definition of Geant4 geometry construction module
+ * @copyright Copyright (c) 2019 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#ifndef ALLPIX_MODULE_GDML_OUTPUT_WRITER_H
+#define ALLPIX_MODULE_GDML_OUTPUT_WRITER_H
+
+#include <memory>
+#include <string>
+
+#include "core/config/Configuration.hpp"
+#include "core/geometry/GeometryManager.hpp"
+#include "core/messenger/Messenger.hpp"
+#include "core/module/Module.hpp"
+
+class G4RunManager;
+
+namespace allpix {
+    /**
+     * @ingroup Modules
+     * @brief Module to construct the Geant4 geometry from the internal geometry
+     *
+     * Creates the world from the information available from the \ref GeometryManager. Then continues with constructing every
+     * detector, building it from the internal detector model. The geometry that is eventually constructed is used to
+     * simulate the charge deposition in the \ref DepositionGeant4Module.
+     */
+    class GDMLOutputWriterModule : public Module {
+    public:
+        /**
+         * @brief Constructor for this unique module
+         * @param config Configuration object for this module as retrieved from the steering file
+         * @param messenger Pointer to the messenger object to allow binding to messages on the bus
+         * @param geo_manager Pointer to the geometry manager, containing the detectors
+         */
+        GDMLOutputWriterModule(Configuration& config, Messenger* messenger, GeometryManager* geo_manager);
+
+        /**
+         * @brief Initializes Geant4 and construct the Geant4 geometry from the internal geometry
+         */
+        void init() override;
+
+    private:
+        GeometryManager* geo_manager_;
+
+        // Geant4 run manager is owned by GeometryBuilderGeant4
+        G4RunManager* run_manager_g4_;
+    };
+} // namespace allpix
+
+#endif /* ALLPIX_MODULE_GDML_OUTPUT_WRITER_H */
---
+++ src/modules/GDMLOutputWriter/README.md
@@ -0,0 +1,17 @@
+# GDMLOutputWriter
+**Maintainer**: Koen van den Brandt (<kbrandt@nikhef.nl>)
+**Status**: Functional
+
+### Description
+Constructs a GDML output of the geometry if added to the chain
+
+
+### Dependencies
+
+This module requires an installation Geant4_GDML.
+
+### Parameters
+
+### Usage
+
+```
