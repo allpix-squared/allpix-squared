724f1279c0e5276042192f6e119d2343925ccb7b
mmoanis <mohamed_moanis@outlook.com>
2019-07-04 13:34:23 +0200

    Synchronize seed initialization between workers

--- src/G4RunManager/RunManager.cpp
+++ src/G4RunManager/RunManager.cpp
@@ -12,6 +12,10 @@
 
 using namespace allpix;
 
+namespace {
+ G4Mutex worker_seed_mutex = G4MUTEX_INITIALIZER;
+}
+
 G4ThreadLocal WorkerRunManager* RunManager::worker_run_manager_ = nullptr;
 
 void RunManager::Run(G4int n_event) {
@@ -20,26 +24,29 @@ void RunManager::Run(G4int n_event) {
         worker_run_manager_ = WorkerRunManager::GetNewInstanceForThread();
     }
 
-    // Draw the nessecary seeds so that each event will be seeded
-    // TODO: maybe we only need to seed the RNG once for a worker run and not for each loop iteration
-    G4RNGHelper* helper = G4RNGHelper::GetInstance();
-    G4int idx_rndm = nSeedsPerEvent*nSeedsUsed;
-    long s1 = helper->GetSeed(idx_rndm), s2 = helper->GetSeed(idx_rndm+1);
+    {
+        G4AutoLock l(&worker_seed_mutex);
+        // Draw the nessecary seeds so that each event will be seeded
+        // TODO: maybe we only need to seed the RNG once for a worker run and not for each loop iteration
+        G4RNGHelper* helper = G4RNGHelper::GetInstance();
+        G4int idx_rndm = nSeedsPerEvent*nSeedsUsed;
+        long s1 = helper->GetSeed(idx_rndm), s2 = helper->GetSeed(idx_rndm+1);
 
-    worker_run_manager_->seedsQueue.push(s1);
-    worker_run_manager_->seedsQueue.push(s2);
+        worker_run_manager_->seedsQueue.push(s1);
+        worker_run_manager_->seedsQueue.push(s2);
 
-    nSeedsUsed++;
+        nSeedsUsed++;
 
-    if(nSeedsUsed==nSeedsFilled) {
-        // The RefillSeeds call will refill the array with 1024 new entries
-        // the number of seeds refilled = numberOfEventToBeProcessed - nSeedsFilled
-        numberOfEventToBeProcessed = nSeedsFilled + 1024;
-        RefillSeeds();
+        if(nSeedsUsed==nSeedsFilled) {
+            // The RefillSeeds call will refill the array with 1024 new entries
+            // the number of seeds refilled = numberOfEventToBeProcessed - nSeedsFilled
+            numberOfEventToBeProcessed = nSeedsFilled + 1024;
+            RefillSeeds();
+        }
+
+        // for book keeping
+        numberOfEventProcessed += n_event;
     }
-
-    // for book keeping
-    numberOfEventProcessed += n_event;
 
     // redirect the call to the correct manager responsible for this thread
     worker_run_manager_->BeamOn(n_event);
