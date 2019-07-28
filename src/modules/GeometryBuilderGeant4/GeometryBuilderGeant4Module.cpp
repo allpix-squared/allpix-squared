2b382d3ff5f6b83538acfcca0d978a1135f17e6d
mmoanis <mohamed_moanis@outlook.com>
2019-07-29 00:21:57 +0200

    GeometryBuilder: Apply clang-format

--- src/modules/GeometryBuilderGeant4/GeometryBuilderGeant4Module.cpp
+++ src/modules/GeometryBuilderGeant4/GeometryBuilderGeant4Module.cpp
@@ -51,15 +51,13 @@ GeometryBuilderGeant4Module::GeometryBuilderGeant4Module(Configuration& config, 
 static void check_dataset_g4(const std::string& env_name) {
     const char* file_name = std::getenv(env_name.c_str());
     if(file_name == nullptr) {
-        throw ModuleError("Geant4 environment variable " + env_name +
-                          " is not set, make sure to source a Geant4 "
-                          "environment with all datasets");
+        throw ModuleError("Geant4 environment variable " + env_name + " is not set, make sure to source a Geant4 "
+                                                                      "environment with all datasets");
     }
     std::ifstream file(file_name);
     if(!file.good()) {
-        throw ModuleError("Geant4 environment variable " + env_name +
-                          " does not point to existing dataset, the Geant4 "
-                          "environment is invalid");
+        throw ModuleError("Geant4 environment variable " + env_name + " does not point to existing dataset, the Geant4 "
+                                                                      "environment is invalid");
     }
     // FIXME: check if file does actually contain a correct dataset
 }
