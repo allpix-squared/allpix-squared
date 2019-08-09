290a0512b63a9773e7bfea842cc550a966507001
mmoanis <mohamed_moanis@outlook.com>
2019-08-09 18:37:50 +0200

    Check if modules can parallelize when MT is requested

--- src/core/module/ModuleManager.cpp
+++ src/core/module/ModuleManager.cpp
@@ -60,6 +60,7 @@ void ModuleManager::load(Messenger* messenger, ConfigManager* conf_manager, Geom
     // Set alias for backward compatibility with the previous keyword for multithreading
     global_config.setDefault("experimental_multithreading", false);
     global_config.setAlias("multithreading", "experimental_multithreading");
+    multithreading_flag_ = global_config.get<bool>("multithreading");
 
     // Store the messenger
     messenger_ = messenger;
@@ -235,9 +236,19 @@ void ModuleManager::load(Messenger* messenger, ConfigManager* conf_manager, Geom
             // Save the identifier in the module
             mod->set_identifier(identifier);
 
+            // Check if module can't run in parallel
+            can_parallelize_ = mod->canParallelize() && can_parallelize_;
+
             // Add the new module to the run list
             modules_.emplace_back(std::move(mod));
             id_to_module_[identifier] = --modules_.end();
+        }
+    }
+
+    // Force MT off for all modules in case one module can't run in parallel
+    if(!can_parallelize_) {
+        for(auto& module : modules_) {
+            module->set_parallelize(false);
         }
     }
     LOG_PROGRESS(STATUS, "LOAD_LOOP") << "Loaded " << configs.size() << " modules";
@@ -588,7 +599,8 @@ void ModuleManager::run(std::mt19937_64& seeder) {
     Configuration& global_config = conf_manager_->getGlobalConfiguration();
     size_t threads_num;
 
-    if(global_config.get<bool>("multithreading")) {
+    // See if we can run in parallel with how many workers
+    if(multithreading_flag_ && can_parallelize_) {
         // Try to fetch a suitable number of workers if multithreading is enabled
         auto available_hardware_concurrency = std::thread::hardware_concurrency();
         if(available_hardware_concurrency > 0u) {
@@ -610,6 +622,14 @@ void ModuleManager::run(std::mt19937_64& seeder) {
                             "29417/7> for more info.";
         }
     } else {
+        // Issue a warning in case MT was requested but we can't actually run in MT
+        if(multithreading_flag_ && !can_parallelize_) {
+            global_config.set<size_t>("multithreading", false);
+            LOG(WARNING)
+                << "Multithreading was requested, however, the current module configuration doesn't support multithreading."
+                   " Turning multithreading OFF!";
+        }
+
         // Default to no additional thread without multithreading
         threads_num = 0;
     }
