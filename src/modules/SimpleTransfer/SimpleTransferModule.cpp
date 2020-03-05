9ae50439fa77a2dd82cb80bb01913336f5944440
Simon Spannagel <simon.spannagel@cern.ch>
2020-03-05 17:43:52 +0100

    SimpleTransfer: rephrase debug message to be more clear

--- src/modules/SimpleTransfer/SimpleTransferModule.cpp
+++ src/modules/SimpleTransfer/SimpleTransferModule.cpp
@@ -95,7 +95,7 @@ void SimpleTransferModule::run(Event* event) {
            max_depth_distance_) {
             LOG(TRACE) << "Skipping set of " << propagated_charge.getCharge() << " propagated charges at "
                        << Units::display(propagated_charge.getLocalPosition(), {"mm", "um"})
-                       << " because their local position is not in implant range";
+                       << " because their local position is not near sensor surface";
             continue;
         }
 
