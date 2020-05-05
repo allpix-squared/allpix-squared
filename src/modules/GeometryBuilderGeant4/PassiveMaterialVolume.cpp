bf10c55b28b992f777827cf8d59c38faf627c296
Paul Schuetze <paul.schuetze@desy.de>
2020-05-05 14:59:03 +0200

    PassiveMaterials: Bounce copyright years

--- src/modules/GeometryBuilderGeant4/PassiveMaterialVolume.cpp
+++ src/modules/GeometryBuilderGeant4/PassiveMaterialVolume.cpp
@@ -2,7 +2,7 @@
  * @file
  * @brief Implements the construction of passive materials in Geant4
  * @remarks Code is based on code from Mathieu Benoit
- * @copyright Copyright (c) 2017-2019 CERN and the Allpix Squared authors.
+ * @copyright Copyright (c) 2017-2020 CERN and the Allpix Squared authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
