e1264da8281e1383852ea231d7a0156dc19fc190
Paul Schuetze <paul.schuetze@desy.de>
2020-05-08 16:56:52 +0200

    GeoConstruction: Couple release of G4cout to log level

--- src/modules/GeometryBuilderGeant4/GeometryConstructionG4.cpp
+++ src/modules/GeometryBuilderGeant4/GeometryConstructionG4.cpp
@@ -198,12 +198,12 @@ void GeometryConstructionG4::check_overlaps() {
     LOG(TRACE) << "Checking overlaps";
     bool overlapFlag = false;
     // Release Geant4 output for better error description
-    RELEASE_STREAM(G4cout);
+    IFLOG(WARNING) { RELEASE_STREAM(G4cout); }
     for(auto volume : (*phys_volume_store)) {
         overlapFlag = volume->CheckOverlaps(1000, 0., false) || overlapFlag;
     }
     // Supress again to prevent further complications
-    SUPPRESS_STREAM(G4cout);
+    IFLOG(WARNING) { SUPPRESS_STREAM(G4cout); }
     if(overlapFlag) {
         LOG(ERROR) << "Overlapping volumes detected.";
     } else {
