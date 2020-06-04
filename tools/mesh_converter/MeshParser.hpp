715bc64c541885b837895005f75961632337aaa6
Simon Spannagel <simon.spannagel@cern.ch>
2020-06-04 14:25:08 +0200

    MeshConverter: add MeshParser base class

---
+++ tools/mesh_converter/MeshParser.hpp
@@ -0,0 +1,36 @@
+#ifndef ALLPIX_MESHPARSER_H
+#define ALLPIX_MESHPARSER_H
+
+#include "MeshElement.hpp"
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace mesh_converter {
+
+    using MeshMap = std::map<std::string, std::vector<Point>>;
+    using FieldMap = std::map<std::string, std::map<std::string, std::vector<Point>>>;
+
+    /**
+     * @brief Parser class to read different data formats
+     */
+    class MeshParser {
+    public:
+        /**
+         * @brief Default constructor
+         */
+        MeshParser() = default;
+
+        // Read the grid
+        virtual MeshMap read_meshes(const std::string& file_name, bool mesh_tree) = 0;
+
+        // Read the electric field
+        virtual FieldMap read_fields(const std::string& file_name) = 0;
+    };
+
+} // namespace mesh_converter
+
+#include "parsers/DFISEParser.hpp"
+
+#endif // ALLPIX_MESHPARSER_H
