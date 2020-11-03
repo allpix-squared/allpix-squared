6568d88ef74e117c236554a81abff257e3105cdd
Simon Spannagel <simon.spannagel@cern.ch>
2020-11-03 18:29:20 +0100

    Git hooks: fix target directory for git version < 2.13. This fixes #187

--- etc/git-hooks/install-hooks.sh
+++ etc/git-hooks/install-hooks.sh
@@ -6,7 +6,7 @@ REPLACE=("pre-commit-clang-format" "pre-push-tag-version")
 WARNING=("pre-commit" "pre-push")
 
 SRCDIR=$(git rev-parse --show-toplevel)/etc/git-hooks
-TGTDIR=$(git rev-parse --absolute-git-dir)/hooks
+TGTDIR=$(readlink -f $(git rev-parse --git-dir))/hooks
 
 ##---
 
