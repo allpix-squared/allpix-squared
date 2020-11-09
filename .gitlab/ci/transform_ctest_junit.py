1fc4491a11d1c74039d45692599f942cb0cdaebd
Simon Spannagel <simon.spannagel@cern.ch>
2020-11-09 06:52:34 +0100

    CI: set XSL template from CLI

--- .gitlab/ci/transform_ctest_junit.py
+++ .gitlab/ci/transform_ctest_junit.py
@@ -8,13 +8,14 @@ if len(sys.argv) != 3:
   sys.exit(1)
 
 input_xml = sys.argv[1]
-output_xml = sys.argv[2]
+input_xsl = sys.argv[2]
+output_xml = sys.argv[3]
 
 print("Reading XML input file")
 dom = et.parse(input_xml)
 
 print("Reading XSLT file")
-xslt = et.parse("ctest-to-junit.xsl")
+xslt = et.parse(input_xsl)
 
 print("Transforming DOM")
 transform = et.XSLT(xslt)
