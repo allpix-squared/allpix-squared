e531b7d85c83675fa9371c9814c9f7e5c082aad1
Simon Spannagel <simon.spannagel@cern.ch>
2020-11-09 13:29:04 +0100

    Multithreading with one worker doesn't make much sense.

--- src/core/module/ModuleManager.cpp
+++ src/core/module/ModuleManager.cpp
@@ -610,8 +610,8 @@ void ModuleManager::run(RandomNumberGenerator& seeder) {
             available_hardware_concurrency -= 1u;
         }
         threads_num = global_config.get<unsigned int>("workers", std::max(available_hardware_concurrency, 1u));
-        if(threads_num == 0) {
-            throw InvalidValueError(global_config, "workers", "number of workers should be strictly more than zero");
+        if(threads_num < 2) {
+            throw InvalidValueError(global_config, "workers", "number of workers should be larger than one");
         }
         LOG(STATUS) << "Multithreading enabled, processing events in parallel on " << threads_num << " worker threads";
 
@@ -634,7 +634,7 @@ void ModuleManager::run(RandomNumberGenerator& seeder) {
     global_config.set<size_t>("workers", threads_num);
 
     // Creates the thread pool
-    LOG(TRACE) << "Initializing thread pool with " << threads_num << " thread";
+    LOG(TRACE) << "Initializing thread pool with " << threads_num << " threads";
     auto initialize_function =
         [log_level = Log::getReportingLevel(), log_format = Log::getFormat(), modules_list = modules_]() {
             // Initialize the threads to the same log level and format as the master setting
