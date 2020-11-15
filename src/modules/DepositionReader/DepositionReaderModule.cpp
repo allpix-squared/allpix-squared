891cf52ca6c0e43df4bf781e3f30d08cb5ed8e3c
Simon Spannagel <simon.spannagel@cern.ch>
2020-11-15 14:52:30 +0100

    DepositionReder: enable_parallelization

--- src/modules/DepositionReader/DepositionReaderModule.cpp
+++ src/modules/DepositionReader/DepositionReaderModule.cpp
@@ -19,6 +19,8 @@ using namespace allpix;
 
 DepositionReaderModule::DepositionReaderModule(Configuration& config, Messenger* messenger, GeometryManager* geo_manager)
     : BufferedModule(config), geo_manager_(geo_manager), messenger_(messenger) {
+    // Enable parallelization of this module if multithreading is enabled
+    enable_parallelization();
 
     config_.setDefault<double>("charge_creation_energy", Units::get(3.64, "eV"));
     config_.setDefault<double>("fano_factor", 0.115);
