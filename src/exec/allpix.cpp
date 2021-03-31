2f9d2e4c66b65916c4a0da7d8db6d6e245015b95
Simon Spannagel <simon.spannagel@cern.ch>
2021-03-31 15:37:24 +0200

    Rephrase message when calling SIGINT

--- src/exec/allpix.cpp
+++ src/exec/allpix.cpp
@@ -52,7 +52,7 @@ void abort_handler(int) {
 void interrupt_handler(int) {
     // Stop the framework if it is loaded
     if(apx_ready) {
-        LOG(STATUS) << "Interrupted! Finishing up queued events...";
+        LOG(STATUS) << "Interrupted! Finishing up active events...";
         apx->terminate();
     }
 }
