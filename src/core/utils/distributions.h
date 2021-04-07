222795dd1f8fb6f935485b8a6a230fc7f02925c3
Simon Spannagel <simon.spannagel@cern.ch>
2021-04-07 18:27:23 +0200

    Fix header

--- src/core/utils/distributions.h
+++ src/core/utils/distributions.h
@@ -1,11 +1,10 @@
 /**
  * @file
- * @brief Random distributions, copied from
- * @copyright Copyright (c) 2007-2018 Free Software Foundation, Inc.
- *
- * This file is part of the GNU ISO C++ Library.  This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3, or (at
- * your option) any later version.
+ * @brief Wrapper for Boost.Random random number distributions used for portability
+ * @copyright Copyright (c) 2021 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
 #ifndef ALLPIX_RANDOM_DISTRIBUTIONS_H
