ea1318ecbc0daeab70e426a529fa31273c0fec62
TasneemSaleem <tasneem.saleem@synchrotron-soleil.fr>
2021-04-21 16:38:44 +0200

    Sur la branche hexagonal-new  Modifications qui seront validées : 	modifié :         src/core/geometry/DetectorModel.hpp 	modifié :         src/core/geometry/GeometryManager.cpp 	nouveau fichier : src/core/geometry/HexagonalPixelDetectorModel.hpp 	modifié :         src/modules/GeometryBuilderGeant4/DetectorConstructionG4.cpp 	modifié :         src/modules/RCEWriter/RCEWriterModule.cpp

--- src/modules/DetectorHistogrammer/DetectorHistogrammerModule.cpp
+++ src/modules/DetectorHistogrammer/DetectorHistogrammerModule.cpp
@@ -15,6 +15,7 @@
 #include <utility>
 
 #include "core/geometry/HybridPixelDetectorModel.hpp"
+#include "core/geometry/HexagonalPixelDetectorModel.hpp"
 #include "core/messenger/Messenger.hpp"
 #include "core/utils/log.h"
 
