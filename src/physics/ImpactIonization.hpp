327cd0abcecfa4635db37755d23fed7c3b472cad
Simon Spannagel <simon.spannagel@cern.ch>
2021-04-29 23:52:54 +0200

    Impact Ionization: add Okuto, Bologna models

--- src/physics/ImpactIonization.hpp
+++ src/physics/ImpactIonization.hpp
@@ -61,6 +61,8 @@ namespace allpix {
      *
      * Formulae 2a for electrons and 2b for holes, temperature dependence equation 3 of
      * https://ieeexplore.ieee.org/document/1677871. Parameter values from text in section III, below formulae.
+     * FIMXE Weightfield2 uses
+     * electron_b_ = 8.4125e4 + 9.98e2 * T
      */
     class Massey : virtual public ImpactIonizationModel {
     public:
@@ -91,14 +93,17 @@ namespace allpix {
 
     /**
      * @ingroup Models
-     * @brief van Overstraeten model for impact ionization
+     * @brief van Overstraeten de Man model for impact ionization
      *
-     * Comment: in the TCAD manual, T0 for the calculation of gamma is never stated for the van Overstraeten-de Man model. It
-     * is given as 300K for the Okuto-Crowell model and is assumed to be also 300K here
+     * Taken from https://www.sciencedirect.com/science/article/pii/0038110170901395; Parametrization according to
+     * Chynoweth's law, parameter values taken from abstract.
+     *
+     * Temperature scaling via Synopsys Sentaurus user manual, but T0 reference value for gamma_ is not entirely clear since
+     * it it never stated explictly. Assuming 300K.
      */
-    class VanOverstraeten : virtual public ImpactIonizationModel {
+    class VanOverstraetenDeMan : virtual public ImpactIonizationModel {
     public:
-        VanOverstraeten(double temperature)
+        VanOverstraetenDeMan(double temperature)
             : gamma_(std::tanh(Units::get(0.063e6, "eV") / (2. * Units::get(8.6173e-5, "eV/K") * 300.)) /
                      std::tanh(Units::get(0.063e6, "eV") / (2. * Units::get(8.6173e-5, "eV/K") * temperature))),
               e_zero_(Units::get(4.0e5, "V/cm")), electron_a_(Units::get(7.03e5, "/cm")),
@@ -134,6 +139,84 @@ namespace allpix {
     };
 
     /**
+     * @ingroup Models
+     * @brief Okuto Crowell model for impact ionization
+     *
+     * Taken from https://www.sciencedirect.com/science/article/pii/0038110175900994. Parametrization according to equations
+     * 7, 8 and 9; Parameter values from Table 1 for silicon
+     */
+    class OkutoCrowell : virtual public ImpactIonizationModel {
+    public:
+        OkutoCrowell(double temperature)
+            : electron_ac_(Units::get(0.426, "/V") * (1. + 3.05e-4 * (temperature - 300))),
+              electron_bd_(Units::get(4.81e5, "V/cm") * (1. + 6.86e-4 * (temperature - 300))),
+              hole_ac_(Units::get(0.243, "/V") * (1. + 5.35e-4 * (temperature - 300))),
+              hole_bd_(Units::get(6.53e5, "V/cm") * (1. + 5.67e-4 * (temperature - 300))) {}
+
+        double operator()(const CarrierType& type, double efield_mag, double threshold, double step) const override {
+            if(std::fabs(efield_mag) < threshold) {
+                return 1.;
+            }
+
+            if(type == CarrierType::ELECTRON) {
+                return std::exp(step * electron_ac_ * efield_mag *
+                                std::exp(-1 * electron_bd_ * electron_bd_ / efield_mag / efield_mag));
+            } else {
+                return std::exp(step * hole_ac_ * efield_mag * std::exp(-1 * hole_bd_ * hole_bd_ / efield_mag / efield_mag));
+            }
+        };
+
+    private:
+        double electron_ac_;
+        double electron_bd_;
+        double hole_ac_;
+        double hole_bd_;
+    };
+
+    /**
+     * @ingroup Models
+     * @brief Bologna model for impact ionization
+     */
+    class Bologna : virtual public ImpactIonizationModel {
+    public:
+        Bologna(double temperature)
+            : electron_a_(Units::get(4.3383, "V") + Units::get(-2.42e-12, "V") * std::pow(temperature, 4.1233)),
+              electron_b_(Units::get(0.235, "V")),
+              electron_c_(Units::get(1.6831e4, "V/cm") + Units::get(4.3796, "V/cm") * temperature +
+                          Units::get(0.13005, "V/cm") * std::pow(temperature, 2)),
+              electron_d_(Units::get(1.2337e6, "V/cm") + Units::get(1.2039e3, "V/cm") * temperature +
+                          Units::get(0.56703, "V/cm") * std::pow(temperature, 2)),
+              hole_a_(Units::get(2.376, "V") + Units::get(1.033e-2, "V") * temperature),
+              hole_b_(Units::get(0.17714, "V") * std::exp(-2.178e-3 * temperature)),
+              hole_c_(Units::get(9.47e-3, "V/cm") * std::pow(temperature, 2.4924)),
+              hole_d_(Units::get(1.4043e6, "V/cm") + Units::get(2.9744e3, "V/cm") * temperature +
+                      Units::get(1.4829, "V/cm") * std::pow(temperature, 2)) {}
+
+        double operator()(const CarrierType& type, double efield_mag, double threshold, double step) const override {
+            if(std::fabs(efield_mag) < threshold) {
+                return 1.;
+            }
+
+            if(type == CarrierType::ELECTRON) {
+                return std::exp(step * efield_mag /
+                                (electron_a_ + electron_b_ * std::exp(electron_d_ / (efield_mag + electron_c_))));
+            } else {
+                return std::exp(step * efield_mag / (hole_a_ + hole_b_ * std::exp(hole_d_ / (efield_mag + hole_c_))));
+            }
+        };
+
+    private:
+        double electron_a_;
+        double electron_b_;
+        double electron_c_;
+        double electron_d_;
+        double hole_a_;
+        double hole_b_;
+        double hole_c_;
+        double hole_d_;
+    };
+
+    /**
      * @brief Wrapper class and factory for impact ionization models.
      *
      * This class allows to store mobility objects independently of the model chosen and simplifies access to the function
@@ -156,7 +239,11 @@ namespace allpix {
             if(model == "massey") {
                 model_ = std::make_unique<Massey>(temperature);
             } else if(model == "overstraeten") {
-                model_ = std::make_unique<VanOverstraeten>(temperature);
+                model_ = std::make_unique<VanOverstraetenDeMan>(temperature);
+            } else if(model == "okuto") {
+                model_ = std::make_unique<OkutoCrowell>(temperature);
+            } else if(model == "bologna") {
+                model_ = std::make_unique<Bologan>(temperature);
             } else if(model == "none") {
                 LOG(INFO) << "No impact ionization model chosen, charge multiplication not simulated";
                 model_ = std::make_unique<None>();
