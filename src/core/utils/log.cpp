52edfb8c315daaa96f879d7d239366af89c1b904
Simon Spannagel <simon.spannagel@cern.ch>
2021-05-07 11:46:09 +0200

    Remove support for C++ < 17

--- src/core/utils/log.cpp
+++ src/core/utils/log.cpp
@@ -438,14 +438,6 @@ bool DefaultLogger::is_terminal(std::ostream& stream) {
  * corresponds to no exception and one to at least one exception.
  */
 int DefaultLogger::get_uncaught_exceptions(bool cons = false) {
-#if __cplusplus > 201402L
-    // we can only do this fully correctly in C++17
     (void)cons;
     return std::uncaught_exceptions();
-#else
-    if(cons) {
-        return 0;
-    }
-    return static_cast<int>(std::uncaught_exception());
-#endif
 }
