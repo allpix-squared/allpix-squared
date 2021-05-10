d5294e4c1c118c9ba18be2c2d2be8b0b00cafc4f
Simon Spannagel <simon.spannagel@cern.ch>
2021-05-10 21:14:59 +0200

    Move binary file check to only place it is used

--- src/core/utils/file.h
+++ src/core/utils/file.h
@@ -19,25 +19,6 @@
 
 #include <sys/stat.h>
 
-namespace allpix {
-
-    /**
-     * @brief Check if the file is a binary file
-     * @param path The path to the file to be checked check
-     * @return True if the file contains null bytes, false otherwise
-     *
-     * This helper function checks the first 256 characters of a file for the occurrence of a nullbyte.
-     * For binary files it is very unlikely not to have at least one. This approach is also used e.g. by diff
-     */
-    inline bool file_is_binary(const std::string& path) {
-        std::ifstream file(path);
-        for(size_t i = 0; i < 256; i++) {
-            if(file.get() == '\0') {
-                return true;
-            }
-        }
-        return false;
-    }
-} // namespace allpix
+namespace allpix {} // namespace allpix
 
 #endif /* ALLPIX_FILE_H */
