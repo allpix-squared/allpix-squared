5c60dad17df089763f66950b159637f389702f32
Radek Privara <radek.privara@cern.ch>
2021-09-01 08:20:39 +0200

    Add calculate_hit_transform explanatory note

--- src/modules/DepositionGeant4/DepositionGeant4Module.hpp
+++ src/modules/DepositionGeant4/DepositionGeant4Module.hpp
@@ -96,6 +96,9 @@ namespace allpix {
          * @brief Calculate hit transformation matrix for a given detector model
          * @param model Detector model
          * @returns Hit transformation matrix pointer
+         *
+         * @note This matrix transforms the Geant4 local coordinate system of the sensor
+         * volume to the APSQ local coordinate system based on the detector model type.
          */
         G4RotationMatrix* calculate_hit_transform(const std::shared_ptr<DetectorModel> model);
 
