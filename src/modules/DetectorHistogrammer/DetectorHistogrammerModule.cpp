31f1a72be139fd991e603dca23ff49277122a093
Simon Spannagel <simon.spannagel@cern.ch>
2021-09-10 12:05:26 +0200

    DetectorHistogrammer: remove unused capture from lambda
    (cherry picked from commit 02d6bdbd59ddba159dfe4526c0172d307c59ebaa)

--- src/modules/DetectorHistogrammer/DetectorHistogrammerModule.cpp
+++ src/modules/DetectorHistogrammer/DetectorHistogrammerModule.cpp
@@ -403,11 +403,10 @@ void DetectorHistogrammerModule::run(Event* event) {
         auto xpixel = static_cast<unsigned int>(std::round(particlePos.x() / pitch.x()));
         auto ypixel = static_cast<unsigned int>(std::round(particlePos.y() / pitch.y()));
 
-        auto matched_cluster =
-            std::find_if(clusters.begin(), clusters.end(), [this, &particlePos, &pitch](const Cluster& clus) {
-                return (std::fabs(clus.getPosition().x() - particlePos.x()) < matching_cut_.x()) &&
-                       (std::fabs(clus.getPosition().y() - particlePos.y()) < matching_cut_.y());
-            });
+        auto matched_cluster = std::find_if(clusters.begin(), clusters.end(), [this, &particlePos](const Cluster& clus) {
+            return (std::fabs(clus.getPosition().x() - particlePos.x()) < matching_cut_.x()) &&
+                   (std::fabs(clus.getPosition().y() - particlePos.y()) < matching_cut_.y());
+        });
 
         // Do we have a match?
         bool matched = matched_cluster != clusters.end();
