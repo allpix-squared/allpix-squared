64a2ec98eb860e4d8a49a1867ca3f0555044ca06
Simon Spannagel <simon.spannagel@cern.ch>
2021-09-24 14:52:34 +0200

    DepositionCosmics: calculate subboxLength from the total setup size

--- src/modules/DepositionCosmics/DepositionCosmicsModule.cpp
+++ src/modules/DepositionCosmics/DepositionCosmicsModule.cpp
@@ -100,7 +100,19 @@ DepositionCosmicsModule::DepositionCosmicsModule(Configuration& config, Messenge
     cry_config << " nParticlesMin " << config_.get<unsigned int>("min_particles") << " nParticlesMax "
                << config_.get<unsigned int>("max_particles");
 
-    // _parmNames[CRYSetup::subboxLength]="subboxLength";
+    // Calculate subbox length required from the maximum coordinates of the setup:
+    LOG(DEBUG) << "Calculating subbox length from setup size";
+    auto min = geo_manager_->getMinimumCoordinate();
+    auto max = geo_manager_->getMaximumCoordinate();
+    auto size = std::max(max.x() - min.x(), max.y() - min.y());
+    auto size_meters = static_cast<unsigned int>(std::ceil(Units::convert(size, "m")));
+    if(size_meters > 300) {
+        throw ModuleError("Size of the setup too large, tabulated data only available for areas up to 300m");
+    }
+    LOG(DEBUG) << "Maximum side length (in x,y): " << Units::display(size, {"mm", "cm", "m"})
+               << ", selecting subbox of size " << size_meters << "m";
+    cry_config << " subboxLength " << size_meters;
+
     // _parmNames[CRYSetup::latitude]="latitude";
     // _parmNames[CRYSetup::date]="date";
     // _parmNames[CRYSetup::xoffset]="xoffset";
