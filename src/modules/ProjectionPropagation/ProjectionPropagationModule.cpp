abc474dee5ba653623c8790a216bcc2152e0f077
Stephan Lachnit <stephanlachnit@debian.org>
2022-01-14 20:09:34 +0100

    Doxyen: add @copyright tag where missing
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- src/modules/ProjectionPropagation/ProjectionPropagationModule.cpp
+++ src/modules/ProjectionPropagation/ProjectionPropagationModule.cpp
@@ -2,7 +2,10 @@
  * @file
  * @brief Implementation of ProjectionPropagation module
  *
- * @copyright MIT License
+ * @copyright Copyright (c) 2017-2020 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
 #include "ProjectionPropagationModule.hpp"
