58bf4273d27ce561f7e2ebee77c2ed486d52feb7
Stephan Lachnit <stephanlachnit@debian.org>
2022-01-14 21:21:10 +0100

    Doxygen: add @file tag where missing
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- src/modules/CapacitiveTransfer/CapacitiveTransferModule.cpp
+++ src/modules/CapacitiveTransfer/CapacitiveTransferModule.cpp
@@ -1,4 +1,6 @@
 /*
+ * @file
+ *
  * @copyright Copyright (c) 2017-2020 CERN and the Allpix Squared authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
