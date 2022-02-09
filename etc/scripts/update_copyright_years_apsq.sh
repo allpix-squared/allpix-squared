f5bc48cc95f16148c70616fa42d10cf464daee4b
Stephan Lachnit <stephanlachnit@debian.org>
2022-02-09 12:30:23 +0100

    Ignore .reuse/dep5 in copyright years script
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- etc/scripts/update_copyright_years_apsq.sh
+++ etc/scripts/update_copyright_years_apsq.sh
@@ -8,7 +8,7 @@ set -e
 UPDATE_SCRIPT=$(dirname $0)/update_copyright_years.py
 APPENDIX="CERN and the Allpix Squared authors"
 
-python3 ${UPDATE_SCRIPT} -v -a "${APPENDIX}" -x 3rdparty/
+python3 ${UPDATE_SCRIPT} -v -a "${APPENDIX}" -x 3rdparty/ -x .reuse/dep5
 
 # update copyright info output allpix exe
 python3 ${UPDATE_SCRIPT} -v -a "${APPENDIX}" -p "std::cout << \"Copyright (c)" -f src/exec/allpix.cpp
