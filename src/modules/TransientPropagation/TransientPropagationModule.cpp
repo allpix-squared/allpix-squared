d10c1a19b27973775b6ae23ae7b6d3d9613c0d14
Simon Spannagel <simon.spannagel@cern.ch>
2022-02-14 14:03:22 +0100

    Recombination: parse model in class itself
    (cherry picked from commit 6531015757232fb70aab14d0a67c75824e2e65e7)

--- src/modules/TransientPropagation/TransientPropagationModule.cpp
+++ src/modules/TransientPropagation/TransientPropagationModule.cpp
@@ -96,11 +96,7 @@ void TransientPropagationModule::initialize() {
     mobility_ = Mobility(config_, detector->hasDopingProfile());
 
     // Prepare recombination model
-    try {
-        recombination_ = Recombination(config_.get<std::string>("recombination_model"), detector->hasDopingProfile());
-    } catch(ModelError& e) {
-        throw InvalidValueError(config_, "recombination_model", e.what());
-    }
+    recombination_ = Recombination(config_, detector->hasDopingProfile());
 
     // Prepare trapping model
     trapping_ = Trapping(config_);
