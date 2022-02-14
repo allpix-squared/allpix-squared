cda4a8358ad853ea3790bdfd281ae8a081492343
Simon Spannagel <simon.spannagel@cern.ch>
2022-02-14 14:33:16 +0100

    HexagonalPixelDetector: add license identifier

--- src/core/geometry/HexagonalPixelDetectorModel.cpp
+++ src/core/geometry/HexagonalPixelDetectorModel.cpp
@@ -2,10 +2,11 @@
  * @file
  * @brief Implementation of hexagonal pixel detector model
  *
- * @copyright Copyright (c) 2021 CERN and the Allpix Squared authors.
+ * @copyright Copyright (c) 2021-2022 CERN and the Allpix Squared authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
+ * SPDX-License-Identifier: MIT
  */
 
 #include "HexagonalPixelDetectorModel.hpp"
--- src/core/geometry/HexagonalPixelDetectorModel.hpp
+++ src/core/geometry/HexagonalPixelDetectorModel.hpp
@@ -2,10 +2,11 @@
  * @file
  * @brief Detector model with hexagonal pixel shape
  *
- * @copyright Copyright (c) 2021 CERN and the Allpix Squared authors.
+ * @copyright Copyright (c) 2021-2022 CERN and the Allpix Squared authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
+ * SPDX-License-Identifier: MIT
  */
 
 #ifndef ALLPIX_HEXAGONAL_PIXEL_DETECTOR_H
