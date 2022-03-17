774de3f0beede88e14c0481450ab7e5cb61a0e2f
Paul Schuetze <paul.schuetze@desy.de>
2022-03-17 15:33:32 +0100

    Add silicon carbide as sensor material
    (cherry picked from commit 8d5a310eb13ca9f44c683eca17a7387d18e93a0c)

--- src/core/geometry/DetectorModel.hpp
+++ src/core/geometry/DetectorModel.hpp
@@ -41,6 +41,7 @@ namespace allpix {
         CADMIUM_TELLURIDE,      ///< Cadmium Telluride
         CADMIUM_ZINC_TELLURIDE, ///< Cadmium Zinc Telluride
         DIAMOND,                ///< Diamond
+        SILICON_CARBIDE,        ///< Silicon Carbide
     };
 
     /**
