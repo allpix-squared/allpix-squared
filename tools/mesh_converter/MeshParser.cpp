aaf11eb9a364aba5d4ed41184cf4ccd2cdefd32c
marco.bomben@cern.ch <marco.bomben@cern.ch>
2022-04-23 12:23:00 +0200

    fixing format
    (cherry picked from commit 9ac6de84e59320059715ec790ac595c8da9fdf97)

--- tools/mesh_converter/MeshParser.cpp
+++ tools/mesh_converter/MeshParser.cpp
@@ -7,7 +7,7 @@ using namespace mesh_converter;
 std::shared_ptr<MeshParser> MeshParser::factory(const allpix::Configuration& config) {
     auto parser = config.get<std::string>("parser", "df-ise");
     std::transform(parser.begin(), parser.end(), parser.begin(), ::tolower);
-    LOG(DEBUG) << "Parser \"" << parser  << "\"\n";
+    LOG(DEBUG) << "Parser \"" << parser << "\"\n";
     if(parser == "df-ise" || parser == "dfise") {
         return std::make_shared<DFISEParser>();
     } else if(parser == "silvaco" || parser == "Silvaco") {
@@ -72,7 +72,7 @@ MeshParser::getField(const std::string& file, const std::string& observable, con
     for(const auto& region : regions) {
         if(field_map_[file].find(region) != field_map_[file].end() &&
            field_map_[file][region].find(observable) != field_map_[file][region].end()) {
-            LOG(DEBUG) << "Region \"" << region  << "\"\n";
+            LOG(DEBUG) << "Region \"" << region << "\"\n";
             field.insert(
                 field.end(), field_map_[file][region][observable].begin(), field_map_[file][region][observable].end());
         } else {
