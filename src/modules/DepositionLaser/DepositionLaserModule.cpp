178a076374e14b91728b49734fdd2aaa0306805d
Daniil Rastorguev <daniil.rastorguev@desy.de>
2022-09-12 14:02:21 +0200

    Laser parameters configuration
    (cherry picked from commit a07e15f08f7ab43346c6132a25618075e93d8760)

--- src/modules/DepositionLaser/DepositionLaserModule.cpp
+++ src/modules/DepositionLaser/DepositionLaserModule.cpp
@@ -23,9 +23,41 @@ DepositionLaserModule::DepositionLaserModule(Configuration& config, Messenger* m
 
     // ... Implement ... (Typically bounds the required messages and optionally sets configuration defaults)
     // Input required by this module
+
+    // Read beam parameters from config
+
+    if(config_.has("source_position")) {
+        source_position_ = config_.get<ROOT::Math::XYZPoint>("source_position");
+        LOG(DEBUG) << "Source position: " << source_position_;
+    } else {
+        throw MissingKeyError("source_position", "DepositionLaser");
+    }
+
+    if(config_.has("beam_direction")) {
+        beam_direction_ = config_.get<ROOT::Math::XYZVector>("beam_direction");
+        LOG(DEBUG) << "Beam direction: " << beam_direction_;
+    } else {
+        throw MissingKeyError("beam_direction", "DepositionLaser");
+    }
+
+    config_.setDefault<double>("beam_waist", 0.02);
+    config_.setDefault<int>("photon_number", 1000);
+
+    beam_waist_ = config_.get<double>("beam_waist");
+    LOG(DEBUG) << "Beam waist: " << Units::convert(beam_waist_, "um") << " um";
+    if(beam_waist_ < 0) {
+        throw InvalidValueError(config_, "beam_waist", "Beam waist should be a positive value!");
+    }
+
+    photon_number_ = config_.get<int>("photon_number");
+    LOG(DEBUG) << "Photon number: " << photon_number_;
+    if(photon_number_ <= 0) {
+        throw InvalidValueError(config_, "photon_number", "Photon number should be a positive nonzero value!");
+    }
 }
 
 void DepositionLaserModule::initialize() {
+
     // Loop over detectors and do something
     std::vector<std::shared_ptr<Detector>> detectors = geo_manager_->getDetectors();
     for(auto& detector : detectors) {
