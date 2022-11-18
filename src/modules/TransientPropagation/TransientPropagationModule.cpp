fc28c3fae4d937d917e6a1b726aff056b3c74738
Simon Spannagel <simon.spannagel@cern.ch>
2022-11-18 01:13:30 +0100

    Transient: do not use Units::convert in hot loop

--- src/modules/TransientPropagation/TransientPropagationModule.cpp
+++ src/modules/TransientPropagation/TransientPropagationModule.cpp
@@ -614,10 +614,8 @@ TransientPropagationModule::propagate(Event* event,
             }
 
             if(output_plots_) {
-                auto inPixel_um_x =
-                    static_cast<double>(Units::convert(position.x() - model_->getPixelCenter(xpixel, ypixel).x(), "um"));
-                auto inPixel_um_y =
-                    static_cast<double>(Units::convert(position.y() - model_->getPixelCenter(xpixel, ypixel).y(), "um"));
+                auto inPixel_um_x = (position.x() - model_->getPixelCenter(xpixel, ypixel).x()) * 1e3;
+                auto inPixel_um_y = (position.y() - model_->getPixelCenter(xpixel, ypixel).y()) * 1e3;
 
                 potential_difference_->Fill(std::fabs(ramo - last_ramo));
                 induced_charge_histo_->Fill(initial_time + runge_kutta.getTime(), induced);
