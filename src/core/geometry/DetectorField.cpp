8248aed28588996aec83d3385749363cb1639b2e
Simon Spannagel <simon.spannagel@cern.ch>
2022-11-18 01:24:23 +0100

    DetectorField: speed up by setting both together

--- src/core/geometry/DetectorField.cpp
+++ src/core/geometry/DetectorField.cpp
@@ -17,12 +17,7 @@ namespace allpix {
      * Vector field template specialization of helper function for field flipping
      */
     template <> void flip_vector_components<ROOT::Math::XYZVector>(ROOT::Math::XYZVector& vec, bool x, bool y) {
-        if(x) {
-            vec.SetX(-vec.x());
-        }
-        if(y) {
-            vec.SetY(-vec.y());
-        }
+        vec.SetXYZ((x ? -vec.x() : vec.x()), (y ? -vec.y() : vec.y()), vec.z());
     }
 
     /*
