53b1ef6f37dd0e664a6d44d80bd8da5d49d7c5c0
drastorg <daniil.rastorguev@cern.ch>
2022-11-22 11:00:50 +0100

    Laser: pulse shape histogram
    (cherry picked from commit 6aadd1620639e17995c21da193f1aaf67b36faed)

--- src/modules/DepositionLaser/DepositionLaserModule.hpp
+++ src/modules/DepositionLaser/DepositionLaserModule.hpp
@@ -140,6 +140,7 @@ namespace allpix {
         Histogram<TH2D> h_intensity_focalplane_{};
         Histogram<TH1D> h_angular_phi_{};
         Histogram<TH1D> h_angular_theta_{};
+        Histogram<TH1D> h_pulse_shape_{};
         std::map<const std::shared_ptr<Detector>, Histogram<TH3D>> h_deposited_charge_shapes_;
     };
 } // namespace allpix
