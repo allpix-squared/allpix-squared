57274b7093b40c118da5f4c53b40c8c888323e6c
Tobias Bisanz <tobias.bisanz@cern.ch>
2023-01-06 13:59:23 +0100

    Fixed linter readability-implicit-bool-conversion, though it's more code noise
    (cherry picked from commit 61051589c55dc10b9ceb363c2a8dda0756d5c524)

--- src/modules/GeometryBuilderGeant4/passive_models/GDMLModel.hpp
+++ src/modules/GeometryBuilderGeant4/passive_models/GDMLModel.hpp
@@ -97,9 +97,10 @@ namespace allpix {
                 // Add offset and rotation to current daughter location
                 G4ThreeVector position_vector = toG4Vector(position_);
                 auto* daughter_rotation = gdml_daughter->GetRotation();
-		//It seems that in the case of a trivial rotation the daughter_rotation can be a nullptr
-                auto* rotation_matrix = (daughter_rotation ? new G4RotationMatrix(*daughter_rotation * *rotation_)
-                                                           : new G4RotationMatrix(*rotation_));
+                // It seems that in the case of a trivial rotation the daughter_rotation can be a nullptr
+                auto* rotation_matrix =
+                    ((daughter_rotation != nullptr) ? new G4RotationMatrix(*daughter_rotation * *rotation_)
+                                                    : new G4RotationMatrix(*rotation_));
                 LOG(TRACE) << "Rotation matrix: " << *rotation_matrix;
                 gdml_daughter->SetTranslation(gdml_daughter->GetTranslation() + position_vector);
                 gdml_daughter->SetRotation(rotation_matrix);
