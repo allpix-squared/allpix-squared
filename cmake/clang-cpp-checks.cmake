93f376d028ea3d9bfb9d50cc9ba76d0abaf6dc74
Simon Spannagel <simon.spannagel@cern.ch>
2023-12-08 12:17:55 +0100

    CMake: search for scripts of the clang-tidy version that was found
    (cherry picked from commit d2914da58bd7e82cdbe18b8d98f897469e2ab19f)

--- cmake/clang-cpp-checks.cmake
+++ cmake/clang-cpp-checks.cmake
@@ -85,7 +85,13 @@ ENDIF()
 FIND_PROGRAM(CLANG_TIDY NAMES "clang-tidy-${CLANG_TIDY_VERSION}" "clang-tidy")
 # Enable clang tidy only if using a clang compiler
 IF(CLANG_TIDY AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    MESSAGE(STATUS "Found ${CLANG_TIDY}")
+
+     EXEC_PROGRAM(
+        ${CLANG_TIDY} ${CMAKE_CURRENT_SOURCE_DIR}
+        ARGS --version
+        OUTPUT_VARIABLE CTIDY_VERSION)
+    STRING(REGEX REPLACE ".* ([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" CLANG_TIDY_MAJOR_VERSION ${CTIDY_VERSION})
+    MESSAGE(STATUS "Found ${CLANG_TIDY} version ${CLANG_TIDY_MAJOR_VERSION}")
 
     # If debug build enabled do automatic clang tidy
     IF(CMAKE_BUILD_TYPE MATCHES Debug)
@@ -111,7 +117,7 @@ IF(CLANG_TIDY AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     GET_FILENAME_COMPONENT(CLANG_DIR ${CLANG_TIDY} DIRECTORY)
     FIND_PROGRAM(
         RUN_CLANG_TIDY
-        NAMES "run-clang-tidy.py" "run-clang-tidy-${CLANG_TIDY_VERSION}.py"
+        NAMES "run-clang-tidy.py" "run-clang-tidy-${CLANG_TIDY_MAJOR_VERSION}.py"
         HINTS /usr/share/clang/ ${CLANG_DIR}/../share/clang/ /usr/bin/)
     IF(RUN_CLANG_TIDY)
         MESSAGE(STATUS "Found ${RUN_CLANG_TIDY}, adding full-code linting targets")
@@ -136,7 +142,7 @@ IF(CLANG_TIDY AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 
     FIND_PROGRAM(
         CLANG_TIDY_DIFF
-        NAMES "clang-tidy-diff.py" "clang-tidy-diff-${CLANG_TIDY_VERSION}.py"
+        NAMES "clang-tidy-diff.py" "clang-tidy-diff-${CLANG_TIDY_MAJOR_VERSION}.py"
         HINTS /usr/share/clang/ ${CLANG_DIR}/../share/clang/ /usr/bin/)
     IF(RUN_CLANG_TIDY)
         # Set target branch and remote to perform the diff against
