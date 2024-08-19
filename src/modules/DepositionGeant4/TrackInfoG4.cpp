e72dfd1b457ed567d67017131f0ccbdf7d160e5e
Simon Spannagel <simon.spannagel@cern.ch>
2024-08-19 12:18:36 +0200

    DepositionGeant4: store G4Process->GetProcessSubType since GetprocessType is only the process category (e.g. "Electromagnetic")
    (cherry picked from commit 435a09bd1c37b7aac7be4b39b74803c31e8910cd)

--- src/modules/DepositionGeant4/TrackInfoG4.cpp
+++ src/modules/DepositionGeant4/TrackInfoG4.cpp
@@ -19,9 +19,9 @@ TrackInfoG4::TrackInfoG4(int custom_track_id, int parent_track_id, const G4Track
       particle_id_(aTrack->GetDynamicParticle()->GetPDGcode()), start_time_(aTrack->GetGlobalTime()),
       initial_g4_vol_name_(aTrack->GetVolume()->GetName()), initial_kin_E_(aTrack->GetKineticEnergy()),
       initial_tot_E_(aTrack->GetTotalEnergy()) {
-    const auto* G4Process = aTrack->GetCreatorProcess();
-    origin_g4_process_type_ = (G4Process != nullptr) ? G4Process->GetProcessType() : -1;
     start_point_ = static_cast<ROOT::Math::XYZPoint>(aTrack->GetPosition());
+    const auto* G4Process = aTrack->GetCreatorProcess();
+    origin_g4_process_type_ = (G4Process != nullptr) ? G4Process->GetProcessSubType() : -1;
     origin_g4_process_name_ = (G4Process != nullptr) ? static_cast<std::string>(G4Process->GetProcessName()) : "none";
 }
 
