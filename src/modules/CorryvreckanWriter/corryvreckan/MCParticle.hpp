93d863b08ddf4363c67bba8d7184367c109f9607
Håkan Wennlöf <h.wennlof@cern.ch>
2025-04-14 16:33:51 +0200

    Fixed the copyright year in some files that have been missed
    Discovered it when adding an issue using a template

--- src/modules/CorryvreckanWriter/corryvreckan/MCParticle.hpp
+++ src/modules/CorryvreckanWriter/corryvreckan/MCParticle.hpp
@@ -2,7 +2,7 @@
  * @file
  * @brief Definition of MCParticle object
  *
- * @copyright Copyright (c) 2017-2020 CERN and the Corryvreckan authors.
+ * @copyright Copyright (c) 2017-2025 CERN and the Corryvreckan authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
