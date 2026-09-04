a20b91cd0fea299fd715cccc08e8b30d824b5a18
Seth R Johnson <johnsonsr@ornl.gov>
2026-09-04 09:45:16 -0400

    Allow user override of ALLPIX_CXX_FLAGS
    Besides reducing configuration noise, this allows user-added
    `-fdiagnostics-color=always` to work.

--- cmake/compiler-flag-checks.cmake
+++ cmake/compiler-flag-checks.cmake
@@ -1,24 +1,26 @@
 # SPDX-FileCopyrightText: 2017-2025 CERN and the Allpix Squared authors
 # SPDX-License-Identifier: MIT
 
-# Check for supported flags and remove unsupported warnings
-FOREACH(flag ${COMPILER_FLAGS})
-    STRING(REPLACE "-" "_" FLAG_WORD ${flag})
-    STRING(REPLACE "+" "P" FLAG_WORD ${FLAG_WORD})
-    STRING(REPLACE "=" "E" FLAG_WORD ${FLAG_WORD})
+IF(NOT DEFINED ALLPIX_CXX_FLAGS)
+    # Check for supported flags and remove unsupported warnings
+    FOREACH(flag ${DEFAULT_ALLPIX_CXX_FLAGS})
+        STRING(REPLACE "-" "_" FLAG_WORD ${flag})
+        STRING(REPLACE "+" "P" FLAG_WORD ${FLAG_WORD})
+        STRING(REPLACE "=" "E" FLAG_WORD ${FLAG_WORD})
 
-    CHECK_CXX_COMPILER_FLAG("${flag}" CXX_FLAG_WORKS_${FLAG_WORD})
-    IF(${CXX_FLAG_WORKS_${FLAG_WORD}})
-        LIST(APPEND ALLPIX_CXX_FLAGS "${flag}")
-    ELSE()
-        MESSAGE(STATUS "NOT adding ${flag} to CXX_FLAGS - unsupported flag")
-    ENDIF()
-ENDFOREACH()
+        CHECK_CXX_COMPILER_FLAG("${flag}" CXX_FLAG_WORKS_${FLAG_WORD})
+        IF(${CXX_FLAG_WORKS_${FLAG_WORD}})
+            LIST(APPEND ALLPIX_CXX_FLAGS "${flag}")
+        ELSE()
+            MESSAGE(STATUS "NOT adding ${flag} to CXX_FLAGS - unsupported flag")
+        ENDIF()
+    ENDFOREACH()
 
-# Reduce Wstrict-overflow level for some GCC versions due to false positives:
-IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    IF(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
-        LIST(APPEND ALLPIX_CXX_FLAGS "-Wstrict-overflow=2")
+    # Reduce Wstrict-overflow level for some GCC versions due to false positives:
+    IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        IF(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0)
+            LIST(APPEND ALLPIX_CXX_FLAGS "-Wstrict-overflow=2")
+        ENDIF()
     ENDIF()
 ENDIF()
 
